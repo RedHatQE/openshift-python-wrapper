f0a1932d1cb394351b898cf67d58f46e00dc6042
Guy Inger <ginger@redhat.com>
2020-08-20 15:13:19 +0000

    add resources: operatorGroup, subscription

---
+++ resources/operator_group.py
@@ -0,0 +1,20 @@
+from resources.resource import NamespacedResource
+
+
+class OperatorGroup(NamespacedResource):
+    api_group = "operators.coreos.com"
+
+    def __init__(
+        self, name, namespace, target_namespaces, teardown=False,
+    ):
+        """
+        Args:
+            targetNamespaces(list): namespaces in which to generate required RBAC access for its member Operators.
+        """
+        super().__init__(name=name, namespace=namespace, teardown=teardown)
+        self.target_namespaces = target_namespaces
+
+    def to_dict(self):
+        res = super()._base_body()
+        res.update({"spec": {"targetNamespaces": self.target_namespaces}})
+        return res
--- resources/subscription.py
+++ resources/subscription.py
@@ -1,5 +1,40 @@
-from .resource import NamespacedResource
+from resources.resource import NamespacedResource
 
 
 class Subscription(NamespacedResource):
     api_group = "operators.coreos.com"
+
+    def __init__(
+        self,
+        name,
+        namespace,
+        source,
+        source_namespace,
+        install_plan_approval,
+        channel,
+        starting_csv,
+        teardown=False,
+    ):
+        super().__init__(name=name, namespace=namespace, teardown=teardown)
+        self.source = source
+        self.source_namespace = source_namespace
+        self.channel = channel
+        self.install_plan_approval = install_plan_approval
+        self.starting_csv = starting_csv
+
+    def to_dict(self):
+        res = super()._base_body()
+        res.update(
+            {
+                "spec": {
+                    "sourceNamespace": self.source_namespace,
+                    "source": self.source,
+                    "name": self.name,
+                    "channel": self.channel,
+                    "installPlanApproval": self.install_plan_approval,
+                    "startingCSV": self.starting_csv,
+                }
+            }
+        )
+
+        return res
