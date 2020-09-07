44408d7eed06e92eb0b9fae5df75abf9f4bf493c
Meni Yakove <myakove@redhat.com>
2020-09-07 11:06:30 +0300

    debug

--- resources/utils.py
+++ resources/utils.py
@@ -126,24 +126,18 @@ def ignore_ssl_exceptions(func):
         timeout = 6
         sleep = 2
         res = None
-        ex = None
+        exception = None
         while timeout > 0:
             try:
                 res = func(*args, **kwargs)
                 timeout = 0
             except urllib3.exceptions.ProtocolError as ex:
-                ex = ex
-                import ipdb
-
-                ipdb.set_trace()
+                exception = ex
                 LOGGER.warning("SSL ProtocolError")
                 time.sleep(sleep)
                 timeout = timeout - sleep
 
         if not res:
-            import ipdb
-
-            ipdb.set_trace()
-            raise urllib3.exceptions.ProtocolError
+            raise urllib3.exceptions.ProtocolError(exception)
 
     return inner
