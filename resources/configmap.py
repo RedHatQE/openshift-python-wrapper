be015a3c2028efde0c97db01e751e1f13bc07aba
Meni Yakove <myakove@redhat.com>
2020-09-16 12:28:38 +0300

    Remove old cert data
    Change-Id: Iea95f6645c0109027e28db1b92d5a687ad383ac0

--- resources/configmap.py
+++ resources/configmap.py
@@ -8,16 +8,11 @@ class ConfigMap(NamespacedResource):
 
     api_version = "v1"
 
-    def __init__(self, name, namespace, cert_name=None, data=None, teardown=True):
+    def __init__(self, name, namespace, data=None, teardown=True):
         super().__init__(name=name, namespace=namespace, teardown=teardown)
-        self.cert_name = cert_name
         self.data = data
 
     def to_dict(self):
         res = super()._base_body()
         res.setdefault("data", {}).update(self.data)
-        if self.cert_name is None:
-            res.update({"data": {"tlsregistry.crt": self.data}})
-        else:
-            res.update({"data": {self.cert_name: self.data}})
         return res
