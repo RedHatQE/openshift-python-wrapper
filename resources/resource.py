d4213dc57179050db564bb21e32e89730e038312
Meni Yakove <myakove@redhat.com>
2021-01-11 11:35:33 +0200

    Resource: add update_replace.
    Change-Id: I167a26d075c7a1205a8945e51bcb7c8349ab3e06

--- resources/resource.py
+++ resources/resource.py
@@ -569,6 +569,17 @@ class Resource(object):
             content_type="application/merge-patch+json",
         )
 
+    def update_replace(self, resource_dict):
+        """
+        Replace resource metadata.
+        Use this to remove existing field. (update() will only update existing fields)
+        """
+        self.api().replace(
+            body=resource_dict,
+            name=self.name,
+            namespace=self.namespace,
+        )
+
     @classmethod
     def get(cls, dyn_client, singular_name=None, *args, **kwargs):
         """
