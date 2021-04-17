e241bae6d1a1a6ac5032e437404512a6e9ab7339
amastbau <amastbau@redhat.com>
2021-04-17 16:41:50 +0300

    with fial flake

--- ocp_resources/mtv.py
+++ ocp_resources/mtv.py
@@ -1,60 +1,82 @@
-import logging
+import abc
 
-from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
+from ocp_resources import resource
+from ocp_resources.resource import NamespacedResource
+from ocp_resources.utils import LOGGER, TimeoutExpiredError, TimeoutSampler
 
+class MTV(abc.ABC, NamespacedResource):
+    """
+    Abstract Class for all Migration ToolKit For Virtualization Resources.
+    """
 
-LOGGER = logging.getLogger(__name__)
+    # the super returns this MTV class name and not the inheriting runtime class as required.
+    @resource.classproperty
+    def kind(cls):
+        return cls.__name__
 
-
-def wait_for_resource_status(
-    mtv_resource,
-    condition_status,
-    condition_type,
-    timeout=600,
-    condition_message=None,
-    condition_reason=None,
-    condition_category=None,
-):
-    LOGGER.info(
-        f"Wait for {mtv_resource.kind} {mtv_resource.name} condition to be {condition_type}"
-    )
-    samples = TimeoutSampler(
-        timeout=timeout,
-        sleep=1,
-        exceptions=TimeoutExpiredError,
-        func=mtv_resource.api().get,
-        field_selector=f"metadata.name=={mtv_resource.name}",
-        namespace=mtv_resource.namespace,
-    )
-    last_condition = None
-    try:
-        for sample in samples:
-            if sample.items:
-                sample_status = sample.items[0].status
-                if sample_status:
-                    current_conditions = sample_status.conditions
-                    for condition in current_conditions:
-                        last_condition = condition
-                        if (
-                            condition.type == condition_type
-                            and condition.status == condition_status
-                            and (
-                                condition.message == condition_message
-                                or condition_message is None
-                            )
-                            and (
-                                condition.condition_reason == condition_reason
-                                or condition_reason is None
-                            )
-                            and (
-                                condition.category == condition_category
-                                or condition_category is None
-                            )
-                        ):
-                            return
-
-    except TimeoutExpiredError:
-        LOGGER.error(
-            msg=f"Last Status Conditions of {mtv_resource.kind} {mtv_resource.name} were: {last_condition}"
+    def __init__(self, name, namespace, client, teardown=600):
+        super().__init__(
+            name=name, namespace=namespace, client=client, teardown=teardown
         )
-        raise
+
+    def _wait_for_resource_status(
+        self,
+        condition_status=None,
+        condition_type=None,
+        condition_message=None,
+        condition_reason=None,
+        condition_category=None,
+        wait_timeout=600,
+    ):
+        LOGGER.info(
+            f"Wait for {self.kind} {self.name} to be "
+            f"condition status:{condition_status or 'any'};"
+            f"condition_type: {condition_type or 'any'};"
+            f"condition_message: {condition_message or 'any'};"
+            f"condition_reason: {condition_reason or 'any'}; "
+            f"condition_category: {condition_category or 'any'}"
+        )
+
+        samples = TimeoutSampler(
+            wait_timeout=wait_timeout,
+            sleep=1,
+            exceptions=TimeoutExpiredError,
+            func=self.api().get,
+            field_selector=f"metadata.name=={self.name}",
+            namespace=self.namespace,
+        )
+        last_condition = None
+        try:
+            for sample in samples:
+                if sample.items:
+                    sample_status = sample.items[0].status
+                    if sample_status:
+                        current_conditions = sample_status.conditions
+                        for condition in current_conditions:
+                            last_condition = condition
+                            if (
+                                (
+                                    condition.type == condition_type
+                                    or condition_type is None
+                                )
+                                and condition.status == condition_status
+                                and (
+                                    condition.message == condition_message
+                                    or condition_message is None
+                                )
+                                and (
+                                    condition.condition_reason == condition_reason
+                                    or condition_reason is None
+                                )
+                                and (
+                                    condition.category == condition_category
+                                    or condition_category is None
+                                )
+                            ):
+                                return
+
+        except TimeoutExpiredError:
+            LOGGER.error(
+                msg=f"Last Status Conditions of {self.kind} {self.name} were: {last_condition}"
+            )
+            raise
