baaca844d81e13f2e990f15af600263cf0bf9063
Meni Yakove <myakove@redhat.com>
2021-07-05 16:02:04 +0300

    bump version to 1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from setuptools import find_packages, setup
 setup(
     name="openshift-python-wrapper",
     license="apache-2.0",
-    download_url="https://github.com/RedHatQE/openshift-python-wrapper/archive/refs/tags/v1.0.tar.gz",
     keywords=["Openshift", "Kubevirt", "CNV"],
     packages=find_packages(),
     include_package_data=True,
