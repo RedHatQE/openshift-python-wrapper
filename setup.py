9e7fcf70afd550651131457b54dd2dbbbbc59600
Meni Yakove <myakove@redhat.com>
2021-07-05 13:55:30 +0300

    Update setup.py for upload to Pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="ocp-python-wrapper",
+    name="openshift-python-wrapper",
+    license="apache-2.0",
+    description="Wrapper around https://github.com/openshift/openshift-restclient-python",
+    author="Meni Yakove, Ruth Netser",
+    author_email="myakove@redhat.com",
+    url="https://github.com/RedHatQE/openshift-python-wrapper",
+    download_url="https://github.com/RedHatQE/openshift-python-wrapper/archive/refs/tags/v1.0.tar.gz",
+    keywords=["Openshift", "Kubevirt", "CNV"],
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
@@ -19,4 +26,8 @@ setup(
         "pbr",
     ],
     python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
 )
