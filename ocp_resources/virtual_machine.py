e00bfca5d269b61757284b5b4b91311fc929d0ff
Sarah Bennert <sbennert@redhat.com>
2021-07-06 07:54:12 -0400

    Allow vmi pause state change timeout to include initial CRD state

--- ocp_resources/virtual_machine.py
+++ ocp_resources/virtual_machine.py
@@ -292,6 +292,15 @@ class VirtualMachineInstance(NamespacedResource):
             func=self.get_vmi_active_condition,
         )
         for sample in samples:
+            # VM in state change
+            # We have commanded a [un]pause condition via the API but the CR has not been updated yet to match.
+            # 'reason' may not exist yet
+            # or
+            # 'reason' may still exist after unpause if the CR has not been updated before we perform this check
+            if (pause and not sample.get("reason")) or (
+                sample.get("reason") == "PausedByUser" and not pause
+            ):
+                continue
             # Paused VM
             if pause and sample["reason"] == "PausedByUser":
                 return
