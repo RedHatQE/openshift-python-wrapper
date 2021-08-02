92f85394fc3ec24b1d86f4654965c87b08e23c40
Meni Yakove <myakove@redhat.com>
2021-08-02 14:14:27 +0300

    release via script

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Usage: ./release.sh v1.5.5
+
+SETUP_CFG="setup.cfg"
+VERSION="v$1"
+OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
+
+# Update setup.cfg with the new version and push to master
+sed -i s/$OLD_VERSION/$VERSION/g $SETUP_CFG
+git commit -a -m"Update version: $VERSION"
+git push origin master
+
+# Create release on Github
+gh release create $VERSION
+
+# Generate release notes
+gren release --override
+
+# Generate and push CHANGELOG.md
+gren changelog --override
+git commit -a -m"Update changelog for version $VERSION"
+git push origin master
+
+# Create branch for the new release
+git checkout -b $VERSION
+git push origin $VERSION
+git checkout master
