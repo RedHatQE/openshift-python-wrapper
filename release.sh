208da72678bd41f28a4bd56126c8f6c5f51ecec3
Meni Yakove <myakove@redhat.com>
2021-08-18 14:18:21 +0300

    release.sh do not run from fork

--- release.sh
+++ release.sh
@@ -5,25 +5,30 @@
 SETUP_CFG="setup.cfg"
 VERSION="$1"
 OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
+REMOTE_ORIGIN=$(grep -A3 '\[remote "origin"\]' .git/config)
+
+if [[ $REMOTE_ORIGIN != *"github.com:RedHatQE/openshift-python-wrapper"* ]]; then
+  echo "This script shouldn't run from forked repo!"
+fi
 
 # Update setup.cfg with the new version and push to master
-sed -i s/$OLD_VERSION/$VERSION/g $SETUP_CFG
-git commit -a -m"Update version: $VERSION"
-git push origin master
-
-# Create release on Github
-gh release create $VERSION
-
-# Generate release notes
-gren release -D prs --override
-
-# Generate and push CHANGELOG.md
-gren changelog --override
-git commit -a -m"Update changelog for version $VERSION"
-git push -f origin master
-
-# Create branch for the new release
-git checkout -b $VERSION
-git push origin $VERSION
-git checkout master
-git pull origin master
+#sed -i s/$OLD_VERSION/$VERSION/g $SETUP_CFG
+#git commit -a -m"Update version: $VERSION"
+#git push origin master
+#
+## Create release on Github
+#gh release create $VERSION
+#
+## Generate release notes
+#gren release -D prs --override
+#
+## Generate and push CHANGELOG.md
+#gren changelog --override
+#git commit -a -m"Update changelog for version $VERSION"
+#git push -f origin master
+#
+## Create branch for the new release
+#git checkout -b $VERSION
+#git push origin $VERSION
+#git checkout master
+#git pull origin master
