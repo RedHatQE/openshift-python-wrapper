aa9662562dfd3d812e2c221330ba793663ee2d56
rnetser <rnetser@redhat.com>
2021-08-22 15:25:39 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-# Usage: ./release.sh v1.5.3
+# Usage: ./release.sh master v1.5.3
 
 SETUP_CFG="setup.cfg"
 BASE_SOURCE_BRANCH="$1"
 VERSION="$2"
 OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
 REMOTE_ORIGIN=$(grep -A3 '\[remote "origin"\]' .git/config)
+if [[ $BASE_SOURCE_BRANCH != "master" ]]; then
+  TARGET_BRANCH="branch-$VERSION"
+else
+  TARGET_BRANCH="master"
+fi
 
 if [[ $REMOTE_ORIGIN != *"github.com:RedHatQE/openshift-python-wrapper"* ]]; then
   echo "This script shouldn't run from forked repo!"
@@ -25,12 +30,12 @@ if [[ $? ]]; then
   exit 1
 fi
 
-git checkout -b $VERSION
+git checkout -b $TARGET_BRANCH
 
-# Update setup.cfg with the new version and push to $VERSION
+# Update setup.cfg with the new version and push to $TARGET_BRANCH
 sed -i s/$OLD_VERSION/$VERSION/g $SETUP_CFG
-git commit -a -m"Update version: $VERSION"
-git push origin $VERSION
+git commit -a -m"Update version: $TARGET_BRANCH"
+git push origin $TARGET_BRANCH
 
 # Create release on Github
 gh release create $VERSION
@@ -41,7 +46,6 @@ gren release -D prs --override
 # Generate and push CHANGELOG.md
 gren changelog --override
 git commit -a -m"Update changelog for version $VERSION"
-git push -f origin $VERSION
+git push -f origin $TARGET_BRANCH
 
-git pull origin $VERSION
-
+git pull origin $TARGET_BRANCH
