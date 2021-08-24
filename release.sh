9421234332c7e58a57739d6548640d89f91143f5
rnetser <rnetser@redhat.com>
2021-08-24 13:38:26 +0300

    Update release.sh and README

--- release.sh
+++ release.sh
@@ -8,11 +8,13 @@ BASE_SOURCE_BRANCH="$1"
 VERSION="$2"
 STRIPPED_VERSION=$(echo $VERSION | sed 's/v//g')
 REMOTE_ORIGIN=$(grep -A3 '\[remote "origin"\]' .git/config)
-if [[ $BASE_SOURCE_BRANCH != "master" ]]; then
+MASTER_BRANCH="master"
+
+if [[ $BASE_SOURCE_BRANCH != $MASTER_BRANCH ]]; then
   TARGET_BRANCH="branch-$VERSION"
 
 else
-  TARGET_BRANCH="master"
+  TARGET_BRANCH=$MASTER_BRANCH
 fi
 
 if [[ $REMOTE_ORIGIN != *"github.com:RedHatQE/openshift-python-wrapper"* ]]; then
@@ -25,29 +27,36 @@ if [[ -z "${GREN_GITHUB_TOKEN}" ]]; then
   exit 1
 fi
 
-# Create branch for the new release
-git checkout -b $TARGET_BRANCH origin/$BASE_SOURCE_BRANCH
-if [[ $? != 0 ]]; then
-  echo "Source branch origin/$BASE_SOURCE_BRANCH does not exist"
+if [[ `git branch --show-current` != $MASTER_BRANCH ]]; then
+  echo "Script must be executed from master branch"
   exit 1
 fi
-OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
 
-# Update setup.cfg with the new version and push to $TARGET_BRANCH
-sed -i s/$OLD_VERSION/$STRIPPED_VERSION/g $SETUP_CFG
-git commit -a -m"Update version: $TARGET_BRANCH"
-git push origin $TARGET_BRANCH
-
-# Create release on Github
-gh release create $VERSION
-
-# Generate release notes
-gren release -D prs --override
-
-# Generate and push CHANGELOG.md
-gren changelog --override
-git commit -a -m"Update changelog for version $VERSION"
-git push -f origin $TARGET_BRANCH
-
-git pull origin $TARGET_BRANCH
-git checkout master
+#
+## Create branch for the new release
+#if [[ $TARGET_BRANCH != $MASTER_BRANCH ]]
+#git checkout -b $TARGET_BRANCH origin/$BASE_SOURCE_BRANCH
+#if [[ $? != 0 ]]; then
+#  echo "Source branch origin/$BASE_SOURCE_BRANCH does not exist"
+#  exit 1
+#fi
+#OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
+#
+## Update setup.cfg with the new version and push to $TARGET_BRANCH
+#sed -i s/$OLD_VERSION/$STRIPPED_VERSION/g $SETUP_CFG
+#git commit -a -m"Update version: $TARGET_BRANCH"
+#git push origin $TARGET_BRANCH
+#
+## Create release on Github
+#gh release create $VERSION
+#
+## Generate release notes
+#gren release -D prs --override
+#
+## Generate and push CHANGELOG.md
+#gren changelog --override
+#git commit -a -m"Update changelog for version $VERSION"
+#git push -f origin $TARGET_BRANCH
+#
+#git pull origin $TARGET_BRANCH
+#git checkout $MASTER_BRANCH
