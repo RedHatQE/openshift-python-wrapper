490658567c5f958ab0cc7b2cf63361baeb1b9d70
Meni Yakove <myakove@redhat.com>
2021-08-29 15:44:24 +0300

    release.sh: refactore code

--- release.sh
+++ release.sh
@@ -3,10 +3,12 @@
 # Usage: ./release.sh master v1.5.3
 # Run from master branch
 
+set -e
+
 SETUP_CFG="setup.cfg"
 BASE_SOURCE_BRANCH="$1"
 VERSION="$2"
-STRIPPED_VERSION=$(echo $VERSION | sed 's/v//g')
+STRIPPED_VERSION="${VERSION//v/}"
 REMOTE_ORIGIN=$(grep -A3 '\[remote "origin"\]' .git/config)
 MASTER_BRANCH="master"
 TARGET_BRANCH="branch-$VERSION" # New branch for the release; formatted: branch-vx.y.z[.i]
@@ -21,27 +23,38 @@ if [[ -z "${GREN_GITHUB_TOKEN}" ]]; then
   exit 1
 fi
 
-if [[ `git branch --show-current` != "$MASTER_BRANCH" ]]; then
+if [[ $(git branch --show-current) != "$MASTER_BRANCH" ]]; then
   echo "Script must be executed from master branch"
   exit 1
 fi
 
-# Create branch for the new release
-git checkout -b $TARGET_BRANCH origin/$BASE_SOURCE_BRANCH
-if [[ $? != 0 ]]; then
+# checkout source branch
+if ! git checkout origin/"$BASE_SOURCE_BRANCH"; then
   echo "Source branch origin/$BASE_SOURCE_BRANCH does not exist"
+  exit 1
+fi
+
+# update source branch
+if ! git pull origin "$BASE_SOURCE_BRANCH"; then
+  echo "Failed to pull latest code from origin/$BASE_SOURCE_BRANCH"
+  exit 1
+fi
+
+# Create branch for the new release
+if ! git checkout -b "$TARGET_BRANCH" origin/"$BASE_SOURCE_BRANCH"; then
+  echo "Failed to create new branch $TARGET_BRANCH"
   exit 1
 fi
 
 OLD_VERSION=$(grep version setup.cfg | awk -F' = ' '{print $2}')
 
 # Update setup.cfg with the new version and push to $TARGET_BRANCH
-sed -i s/$OLD_VERSION/$STRIPPED_VERSION/g $SETUP_CFG
+sed -i s/"$OLD_VERSION"/"$STRIPPED_VERSION"/g "$SETUP_CFG"
 git commit -a -m"Update version: $TARGET_BRANCH"
-git push origin $TARGET_BRANCH
+git push origin "$TARGET_BRANCH"
 
 # Create release on Github
-gh release create $VERSION
+gh release create "$VERSION"
 
 # Generate release notes
 gren release -D prs --override
@@ -49,7 +62,7 @@ gren release -D prs --override
 # Generate and push CHANGELOG.md
 gren changelog --override
 git commit -a -m"Update changelog for version $VERSION"
-git push -f origin $TARGET_BRANCH
+git push -f origin "$TARGET_BRANCH"
 
-git pull origin $TARGET_BRANCH
-git checkout $MASTER_BRANCH
+git pull origin "$TARGET_BRANCH"
+git checkout "$MASTER_BRANCH"
