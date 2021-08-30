b03b55b1d90c9cd6af634ef0f72c327f5e9afc06
rnetser <rnetser@redhat.com>
2021-08-30 11:36:44 +0300

    Add VM printableStatus

--- ocp_resources/event.py
+++ ocp_resources/event.py
@@ -1,45 +1,41 @@
 import logging
 
-from ocp_resources.resource import NamespacedResource
+from ocp_resources.resource import Resource
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-class Event(NamespacedResource):
+class Event:
     """
     Allow read and remove K8s events.
     """
 
-    api_version = NamespacedResource.ApiVersion.V1
+    api_version = Resource.ApiVersion.V1
 
     @classmethod
     def get(
         cls,
         dyn_client,
-        name=None,
         namespace=None,
+        name=None,
         label_selector=None,
         field_selector=None,
         resource_version=None,
         timeout=None,
     ):
         """
+        get - retrieves K8s events.
+        :param dyn_client: K8s client; mandatory
+        :param namespace: event namespace; optional
+        :param name: event name; optional
+        :param label_selector: filter events by labels; comma separated string of key=value; optional
+        :param field_selector: filter events by event fields; comma separated string of key=value; optional
+        :param resource_version: filter events by their resource's version; optional
+        :param timeout: timeout; optional
+        :return: list of event objects
 
-        Args:
-            dyn_client (obj): K8s client
-            namespace (str, optionl): event namespace
-            name (str, optionl): event name
-            label_selector (str, optionl): filter events by labels; comma separated string of key=value
-            field_selector (str, optional): filter events by event fields; comma separated string of key=value
-            resource_version (str, optional): filter events by their resource's version
-            timeout (int, optional):
-
-        Returns:
-            list of event objects
-
-        Example:
-            Reading all CSV Warning events in namespace "my-namespace", with reason of "AnEventReason"
+        example: reading all CSV Warning events in namespace "my-namespace", with reason of "AnEventReason"
               for event in Event.get(
                   default_client,
                   namespace="my-namespace",
@@ -48,7 +44,9 @@ class Event(NamespacedResource):
               ):
                 print(event.object)
         """
-        LOGGER.info(
+
+        LOGGER.info("Reading events")
+        LOGGER.debug(
             f"get events parameters: namespace={namespace}, name={name}, label_selector={label_selector}, "
             f"field_selector='{field_selector}', resource_version={resource_version}, timeout={timeout}"
         )
@@ -78,25 +76,25 @@ class Event(NamespacedResource):
         timeout=None,
     ):
         """
-        Deletes K8s events. For example, to cleanup events before test, in order to not get old events in
-        the test, in order to prevent false positive test.
+        delete_events - delete K8s events. For example, to cleanup events before test, in order to not get old events in
+                        the test, in order to prevent false positive test.
+        :param dyn_client: K8s client; mandatory
+        :param namespace: event namespace; optional
+        :param name: event name; optional
+        :param label_selector: filter events by labels; comma separated string of key=value; optional
+        :param field_selector: filter events by event fields; comma separated string of key=value; optional
+        :param resource_version: filter events by their resource's version; optional
+        :param timeout: timeout; optional
+        :return: list of event objects
 
-        Args:
-            dyn_client (obj): K8s client
-            namespace (str, optional): event namespace
-            name (str, optional): event name
-            label_selector (str, optional): filter events by labels; comma separated string of key=value; optional
-            field_selector (str, optional): filter events by event fields; comma separated string of key=value
-            resource_version (str, optional): filter events by their resource's version
-            timeout (int):
+        example: deleting all the event with a reason of "AnEventReason", from "my-namespace" namespace
 
-        Example:
-            Deleting all the event with a reason of "AnEventReason", from "my-namespace" namespace
-            @pytest.fixture()
-            def delete_events_before_test(default_client):
-            Event.delete_events(default_client, namespace=my-namespace, field_selector="reason=AnEventReason")
+        @pytest.fixture()
+        def delete_events_before_test(default_client):
+          Event.delete_events(default_client, namespace=my-namespace, field_selector="reason=AnEventReason")
         """
-        LOGGER.info(
+        LOGGER.info("Deleting events")
+        LOGGER.debug(
             f"delete_events parameters: namespace={namespace}, name={name}, label_selector={label_selector}, "
             f"field_selector='{field_selector}', resource_version={resource_version}, timeout={timeout}"
         )
