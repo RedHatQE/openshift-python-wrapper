f56317f3dfbb0b744614960d4a02007b79176d96
Meni Yakove <myakove@redhat.com>
2021-12-21 19:58:32 +0200

    set yaml log as debug

--- ocp_resources/datavolume.py
+++ ocp_resources/datavolume.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import logging
-
 from ocp_resources.constants import TIMEOUT_4MINUTES
+from ocp_resources.logger import get_logger
 from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
 from ocp_resources.resource import NamespacedResource, Resource
 from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
 
 
-LOGGER = logging.getLogger(__name__)
+LOGGER = get_logger(name=__name__)
 
 
 class DataVolume(NamespacedResource):
