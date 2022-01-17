de4201dcccf478ea7006c0c57c74b3cdc5e9e72a
azavalko <azavalko@redhat.com>
2022-01-17 11:10:53 +0200

    body_selector

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -389,7 +389,7 @@ class Resource:
         self.delete_timeout = delete_timeout
         self.node_selector = node_selector
         self.node_selector_labels = node_selector_labels
-        self._node_selector = self._set_node_selector()
+        self.body_node_selector = self._set_node_selector()
 
     def _set_node_selector(self):
         if self.node_selector:
