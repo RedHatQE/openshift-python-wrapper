4246e2d1750724d4fb1aaee7590102336d98e4da
Meni Yakove <myakove@redhat.com>
2022-01-30 13:00:34 +0200

    Remove DuplicateFilter

--- ocp_resources/logger.py
+++ ocp_resources/logger.py
@@ -5,27 +5,6 @@ from logging.handlers import RotatingFileHandler
 from colorlog import ColoredFormatter
 
 
-LOGGER = logging.getLogger(__name__)
-
-
-class DuplicateFilter(logging.Filter):
-    def filter(self, record):
-        repeated_number_exists = getattr(self, "repeated_number", None)
-        current_log = (record.module, record.levelno, record.msg)
-        if current_log != getattr(self, "last_log", None):
-            self.last_log = current_log
-            if repeated_number_exists:
-                LOGGER.warning(f"Last log repeated {self.repeated_number} times.")
-
-            self.repeated_number = 0
-            return True
-        if repeated_number_exists:
-            self.repeated_number += 1
-        else:
-            self.repeated_number = 1
-        return False
-
-
 def get_logger(name):
     log_level = os.environ.get("OPENSHIFT_PYTHON_WRAPPER_LOG_LEVEL", "INFO")
     log_file = os.environ.get(
@@ -50,11 +29,9 @@ def get_logger(name):
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(fmt=log_formatter)
-    console_handler.addFilter(filter=DuplicateFilter())
 
     logger_obj.addHandler(hdlr=console_handler)
     logger_obj.setLevel(level=log_level)
-    logger_obj.addFilter(filter=DuplicateFilter())
 
     if log_file:
         log_handler = RotatingFileHandler(
