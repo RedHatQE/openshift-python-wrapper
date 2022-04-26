1b678e3c7b1c37493401c088057da1c0235fa259
azavalko <azavalko@redhat.com>
2022-04-26 14:45:03 +0300

    Allow creating ClusterRoleBinding with parameters

--- ocp_resources/cluster_role_binding.py
+++ ocp_resources/cluster_role_binding.py
@@ -9,3 +9,27 @@ class ClusterRoleBinding(Resource):
     """
 
     api_group = Resource.ApiGroup.RBAC_AUTHORIZATION_K8S_IO
+
+    def __init__(
+        self,
+        name=None,
+        cluster_role=None,
+        subjects=[],
+    ):
+        super().__init__(name=name)
+        self.cluster_role = cluster_role
+        self.subjects = subjects
+        self.res = None
+
+    def to_dict(self):
+
+        self.res = super().to_dict()
+        self.res.setdefault("roleRef", {})
+        self.res["roleRef"] = {
+            "apiGroup": self.api_group,
+            "kind": "ClusterRole",
+            "name": self.cluster_role,
+        }
+        self.res.setdefault("subjects", self.subjects)
+
+        return self.res
