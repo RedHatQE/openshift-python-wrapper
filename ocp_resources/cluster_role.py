bce0dc761efbed3a83d09748209aefc1fc934d57
azavalko <azavalko@redhat.com>
2022-04-27 21:11:45 +0300

    Allow creating ClusterRole with multiple rules

--- ocp_resources/cluster_role.py
+++ ocp_resources/cluster_role.py
@@ -33,19 +33,53 @@ class ClusterRole(Resource):
         self.api_groups = api_groups
         self.permissions_to_resources = permissions_to_resources
         self.verbs = verbs
+        self.desired_state = {"rules": []}
+        self.res = None
 
     def to_dict(self):
-        res = super().to_dict()
+        self.res = super().to_dict()
         if self.yaml_file:
-            return res
+            return self.res
 
-        rules = {}
-        if self.api_groups:
-            rules["apiGroups"] = self.api_groups
         if self.permissions_to_resources:
-            rules["resources"] = self.permissions_to_resources
-        if self.verbs:
-            rules["verbs"] = self.verbs
-        if rules:
-            res["rules"] = [rules]
-        return res
+            self.res = self.add_rule(
+                api_groups=self.api_groups,
+                permissions_to_resources=self.permissions_to_resources,
+                verbs=self.verbs,
+            )
+
+        return self.res
+
+    def add_rule(
+        self,
+        api_groups=None,
+        permissions_to_resources=None,
+        verbs=None,
+    ):
+        if not self.res:
+            self.res = self.to_dict()
+
+        rule = {}
+        if api_groups:
+            rule["apiGroups"] = api_groups
+        if permissions_to_resources:
+            rule["resources"] = permissions_to_resources
+        if verbs:
+            rule["verbs"] = verbs
+        if rule:
+            self.set_rule(rule=rule)
+
+        return self.res
+
+    def set_rule(self, rule):
+        if not self.res:
+            self.res = super().to_dict()
+        # Drop the rule if it's already in the list
+        rules = [
+            current_rule
+            for current_rule in self.desired_state["rules"]
+            if rule["resources"] != current_rule["resources"]
+        ]
+        rules.append(rule)
+        self.desired_state["rules"] = rules
+        self.res["rules"] = self.desired_state["rules"]
