973b3fa4aeed42c981f9f532ee65f49fdde26648
Meni Yakove <myakove@gmail.com>
2022-05-16 19:24:36 +0300

    add SUBRESOURCES_KUBEVIRT_IO

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -328,6 +328,7 @@ class Resource:
         SSP_KUBEVIRT_IO = "ssp.kubevirt.io"
         STORAGE_K8S_IO = "storage.k8s.io"
         STORAGECLASS_KUBERNETES_IO = "storageclass.kubernetes.io"
+        SUBRESOURCES_KUBEVIRT_IO = "subresources.kubevirt.io"
         TEMPLATE_KUBEVIRT_IO = "template.kubevirt.io"
         TEMPLATE_OPENSHIFT_IO = "template.openshift.io"
         UPLOAD_CDI_KUBEVIRT_IO = "upload.cdi.kubevirt.io"
