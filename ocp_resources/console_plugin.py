4542e656b2ae7b0e6c863b961d227507ae62e072
dbasunag <82394005+dbasunag@users.noreply.github.com>
2022-05-24 04:39:20 -0400

    Add new resource console_plugin (#551)
    * Add new resource console_plugin
    
    * Removed api_version

---
+++ ocp_resources/console_plugin.py
@@ -0,0 +1,9 @@
+from ocp_resources.resource import Resource
+
+
+class ConsolePlugin(Resource):
+    """
+    ConsolePlugin object.
+    """
+
+    api_group = Resource.ApiGroup.CONSOLE_OPENSHIFT_IO
