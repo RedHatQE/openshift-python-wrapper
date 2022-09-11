88f674151bb931f0bce04c42dc3643fbf931e14c
Jenia Peimer <86722603+jpeimer@users.noreply.github.com>
2022-09-11 12:20:12 +0300

    Add VMExport resource (#799)

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -256,6 +256,7 @@ class Resource:
         CONSOLE_OPENSHIFT_IO = "console.openshift.io"
         DATA_IMPORT_CRON_TEMPLATE_KUBEVIRT_IO = "dataimportcrontemplate.kubevirt.io"
         EVENTS_K8S_IO = "events.k8s.io"
+        EXPORT_KUBEVIRT_IO = "export.kubevirt.io"
         FLAVOR_KUBEVIRT_IO = "flavor.kubevirt.io"
         FORKLIFT_KONVEYOR_IO = "forklift.konveyor.io"
         INSTANCE_TYPE_KUBEVIRT_IO = "instancetype.kubevirt.io"
