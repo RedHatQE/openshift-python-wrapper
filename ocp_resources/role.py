d6ef5c6f867e6c2b73cfa30b63c0ba1d9c05c55c
‪Anat Wax <46938838+Anatw@users.noreply.github.com>
2022-12-19 15:58:24 +0200

    Add functionality to role.py and refactor clusetr_role.py (#994)
    - Refactor cluster_role.py.
     - Add common code to utils.py.
     - Add an example to the README.md on how to add multiple
    rules to a single Role/ClusterRole.
    
    Signed-off-by: Anat Wax <awax@redhat.com>

--- ocp_resources/role.py
+++ ocp_resources/role.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-
+#  API reference:
+#       https://docs.openshift.com/container-platform/4.11/rest_api/rbac_apis/role-rbac-authorization-k8s-io-v1.html
+from ocp_resources.constants import TIMEOUT_4MINUTES
 from ocp_resources.resource import NamespacedResource
 
 
@@ -9,3 +11,46 @@ class Role(NamespacedResource):
     """
 
     api_group = NamespacedResource.ApiGroup.RBAC_AUTHORIZATION_K8S_IO
+
+    def __init__(
+        self,
+        name=None,
+        namespace=None,
+        client=None,
+        rules=None,
+        teardown=True,
+        yaml_file=None,
+        delete_timeout=TIMEOUT_4MINUTES,
+        **kwargs,
+    ):
+        """
+        Args:
+            name (str): ClusterRole name.
+            namespace (str): Namespace name.
+            client (DynamicClient): DynamicClient to use.
+            rules (list): list of dicts of rules. In the dict:
+                permissions_to_resources (list): List of string with resource/s to which you want to add permissions to.
+                Verbs (list): Determine the action/s (permissions) applicable on a specific resource.
+                    Available verbs per resource can be seen with the command 'oc api-resources --sort-by name -o wide'
+            teardown (bool, default: True): Indicates if this resource would need to be deleted.
+            yaml_file (yaml, default: None): yaml file for the resource.
+            delete_timeout (int, default: 4 minutes): timeout associated with delete action.
+        """
+        super().__init__(
+            client=client,
+            namespace=namespace,
+            name=name,
+            teardown=teardown,
+            yaml_file=yaml_file,
+            delete_timeout=delete_timeout,
+            **kwargs,
+        )
+        self.rules = rules
+
+    def to_dict(self):
+        if not self.rules and not self.yaml_file:
+            raise ValueError("must send rules or yaml_file")
+        if not self.res:
+            super().to_dict()
+        if not self.yaml_file:
+            self.res["rules"] = self.rules
