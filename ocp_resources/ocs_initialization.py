f1535195fdd112c56f7052203f423dc2438abd2e
Anton Asserzon <aasserzo@redhat.com>
2023-01-17 12:34:41 +0200

    added mandatory __init__ and to_dict functions

--- ocp_resources/ocs_initialization.py
+++ ocp_resources/ocs_initialization.py
@@ -1,3 +1,4 @@
+from ocp_resources.constants import TIMEOUT_4MINUTES
 from ocp_resources.resource import NamespacedResource
 
 
@@ -7,3 +8,28 @@ class OCSInitialization(NamespacedResource):
     """
 
     api_group = NamespacedResource.ApiGroup.OCS_OPENSHIFT_IO
+
+    def __init__(
+        self,
+        name=None,
+        namespace=None,
+        client=None,
+        teardown=False,
+        privileged_client=None,
+        yaml_file=None,
+        delete_timeout=TIMEOUT_4MINUTES,
+        **kwargs,
+    ):
+        super().__init__(
+            name=name,
+            namespace=namespace,
+            client=client,
+            teardown=teardown,
+            privileged_client=privileged_client,
+            yaml_file=yaml_file,
+            delete_timeout=delete_timeout,
+            **kwargs,
+        )
+
+    def to_dict(self):
+        super().to_dict()
