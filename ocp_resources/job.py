0bb94723785c9a6b179dc569f10991ca43b86c7a
‪Anat Wax <46938838+Anatw@users.noreply.github.com>
2023-03-05 11:12:25 +0200

    Refactor job and daemonset resources (#1101)
    - Add args to the docstring.
    - Allow background propagation policy in delete().
    - Refactor daemonset.py delete() to return the result.
    
    Signed-off-by: Anat Wax <awax@redhat.com>

--- ocp_resources/job.py
+++ ocp_resources/job.py
@@ -1,3 +1,5 @@
+import kubernetes
+
 from ocp_resources.constants import TIMEOUT_4MINUTES
 from ocp_resources.resource import NamespacedResource
 
@@ -5,6 +7,23 @@ from ocp_resources.resource import NamespacedResource
 class Job(NamespacedResource):
     """
     Job object.
+
+    Args:
+        name (str): Job name.
+        namespace (str): Namespace name.
+        client (DynamicClient): Dynamic client for connecting to a remote cluster.
+        teardown (bool): Indicates if this resource would need to be deleted.
+        privileged_client (DynamicClient): Instance of Dynamic client.
+        yaml_file (str): Yaml file for the resource.
+        delete_timeout (int): Timeout associated with delete action.
+        backoff_limit (int): The number of retries for a job.
+        restart_policy (str): The restart policy of the pod.
+        service_account (str): Optional. Service account name.
+        containers (list): List of containers belonging to the pod that the job will create.
+        background_propagation_policy (str): Control how object dependents will be deleted when an object is
+                deleted (for example the pods left behind when you delete a Job). Options are: "Background",
+                "Foreground" and "Orphan". Read more here:
+                    https://kubernetes.io/docs/concepts/architecture/garbage-collection/#cascading-deletion
     """
 
     api_group = NamespacedResource.ApiGroup.BATCH
@@ -25,6 +44,7 @@ class Job(NamespacedResource):
         restart_policy=None,
         service_account=None,
         containers=None,
+        background_propagation_policy=None,
         **kwargs,
     ):
         super().__init__(
@@ -41,6 +61,7 @@ class Job(NamespacedResource):
         self.backoff_limit = backoff_limit
         self.service_account = service_account
         self.containers = containers
+        self.background_propagation_policy = background_propagation_policy
 
     def to_dict(self):
         super().to_dict()
@@ -63,3 +84,21 @@ class Job(NamespacedResource):
                     self.res["spec"]["template"]["spec"][
                         "restartPolicy"
                     ] = self.restart_policy
+
+    def delete(self, wait=False, timeout=TIMEOUT_4MINUTES, body=None):
+        """
+        Delete Job object
+
+        Args:
+            wait (bool): True to wait for Job to be deleted.
+            timeout (int): Time to wait for resource deletion.
+            body (dict): Content to send to delete().
+
+        Returns:
+            bool: True if delete succeeded, False otherwise.
+        """
+        if not body and self.background_propagation_policy:
+            body = kubernetes.client.V1DeleteOptions(
+                propagation_policy=self.background_propagation_policy
+            )
+        return super().delete(wait=wait, timeout=timeout, body=body)
