6fa1ba7caaff116c3bc204f90c00a0fe2781e19e
dalia-frank <dafrank@redhat.com>
2023-07-03 14:13:09 +0300

    Update StorageClass class

--- ocp_resources/storage_class.py
+++ ocp_resources/storage_class.py
@@ -45,3 +45,57 @@ class StorageClass(Resource):
         IS_DEFAULT_CLASS = (
             f"{Resource.ApiGroup.STORAGECLASS_KUBERNETES_IO}/is-default-class"
         )
+
+    class ReclaimPolicy:
+        DELETE = "Delete"
+        RETAIN = "Retain"
+
+    def __init__(
+        self,
+        name=None,
+        client=None,
+        teardown=True,
+        provisioner=None,
+        reclaim_policy=None,
+        volume_binding_mode=None,
+        allow_volume_expansion=None,
+        parameters=None,
+        **kwargs,
+    ):
+        """
+        StorageClass object
+
+        Args:
+            name (str): StorageClass name.
+            client (DynamicClient): DynamicClient to use.
+            teardown (bool): Indicates if this resource would need to be deleted.
+            provisioner (str): The provisioner of the storage class
+            reclaim_policy (str): Can be either "Delete" or "Retain"
+            volume_binding_mode (str): When volume binding and dynamic provisioning should occur
+            allow_volume_expansion (bool): True for allowing the volume expansion
+            parameters (list): Describe volumes belonging to the storage class.
+        """
+        super().__init__(
+            name=name,
+            client=client,
+            teardown=teardown,
+            **kwargs,
+        )
+        self.provisioner = provisioner
+        self.reclaim_policy = reclaim_policy
+        self.volume_binding_mode = volume_binding_mode
+        self.allow_volume_expansion = allow_volume_expansion
+        self.parameters = parameters
+
+    def to_dict(self):
+        super().to_dict()
+        if not self.yaml_file:
+            self.res.update({"provisioner": self.provisioner})
+            if self.reclaim_policy:
+                self.res.update({"reclaimPolicy": self.reclaim_policy})
+            if self.volume_binding_mode:
+                self.res.update({"volumeBindingMode": self.volume_binding_mode})
+            if self.allow_volume_expansion:
+                self.res.update({"allowVolumeExpansion": self.allow_volume_expansion})
+            if self.parameters:
+                self.res.update({"parameters": self.parameters})
