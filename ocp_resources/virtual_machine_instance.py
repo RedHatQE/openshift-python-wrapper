7628538cf207b3f65e5246602a4cc7fbd8f71e3a
Vasiliy Sibirskiy <vsibirsk@redhat.com>
2023-08-10 02:03:00 +0300

    add more logging to vmi.wait_until_running()

--- ocp_resources/virtual_machine_instance.py
+++ ocp_resources/virtual_machine_instance.py
@@ -115,6 +115,7 @@ class VirtualMachineInstance(NamespacedResource):
             TimeoutExpiredError: If VMI failed to run.
         """
         try:
+            self.logger.info(f"VMI status before wait: {self.instance.status.phase}")
             self.wait_for_status(
                 status=self.Status.RUNNING, timeout=timeout, stop_status=stop_status
             )
@@ -124,6 +125,9 @@ class VirtualMachineInstance(NamespacedResource):
 
             virt_pod = self.virt_launcher_pod
             if virt_pod:
+                self.logger.error(
+                    f"Status of virt-launcher pod {virt_pod.name}: {virt_pod.status}"
+                )
                 self.logger.debug(f"{virt_pod.name} *****LOGS*****")
                 self.logger.debug(virt_pod.log(container="compute"))
 
