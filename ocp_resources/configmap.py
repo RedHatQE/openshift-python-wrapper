7705a7914123e2478278755e888cc6dce931823a
oharan2 <oharan@redhat.com>
2023-09-06 13:03:14 +0300

    Update configmap class constructor and documentation

--- ocp_resources/configmap.py
+++ ocp_resources/configmap.py
@@ -1,34 +1,23 @@
-from ocp_resources.constants import TIMEOUT_4MINUTES
 from ocp_resources.resource import NamespacedResource
 
 
 class ConfigMap(NamespacedResource):
     """
-    Configmap object
+    https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.26/#configmap-v1-core
     """
 
     api_version = NamespacedResource.ApiVersion.V1
 
     def __init__(
         self,
-        name=None,
-        namespace=None,
         data=None,
-        teardown=True,
-        client=None,
-        yaml_file=None,
-        delete_timeout=TIMEOUT_4MINUTES,
         **kwargs,
     ):
-        super().__init__(
-            name=name,
-            namespace=namespace,
-            client=client,
-            teardown=teardown,
-            yaml_file=yaml_file,
-            delete_timeout=delete_timeout,
-            **kwargs,
-        )
+        """
+        Args:
+            data (dict): key-value configuration pairs.
+        """
+        super().__init__(**kwargs)
         self.data = data
 
     def to_dict(self):
