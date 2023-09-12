4ce48d867085291abf99a6c3a208f8fdcf27bab1
oharan2 <oharan@redhat.com>
2023-09-12 12:46:14 +0300

    Update catalog_source class constructor, documentation and add 'to_dict' parameters validation (#1412)
    * Update catalog_source class constructor and documentation
    
    * Add to_dict parameter validtion
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix logic
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Meni Yakove <441263+myakove@users.noreply.github.com>

--- ocp_resources/catalog_source.py
+++ ocp_resources/catalog_source.py
@@ -1,34 +1,32 @@
-from ocp_resources.constants import TIMEOUT_4MINUTES
 from ocp_resources.resource import NamespacedResource
 
 
 class CatalogSource(NamespacedResource):
+    """
+    https://olm.operatorframework.io/docs/concepts/crds/catalogsource/
+    """
+
     api_group = NamespacedResource.ApiGroup.OPERATORS_COREOS_COM
 
     def __init__(
         self,
-        name=None,
-        namespace=None,
-        client=None,
         source_type=None,
         image=None,
         display_name=None,
         publisher=None,
-        teardown=True,
-        yaml_file=None,
-        delete_timeout=TIMEOUT_4MINUTES,
         update_strategy_registry_poll_interval=None,
         **kwargs,
     ):
-        super().__init__(
-            name=name,
-            namespace=namespace,
-            client=client,
-            teardown=teardown,
-            yaml_file=yaml_file,
-            delete_timeout=delete_timeout,
-            **kwargs,
-        )
+        """
+        Args:
+            source_type (str): Name of the source type.
+            image (str): Image index for the catalog.
+            display_name (str): Display name for the catalog in the web console and CLI.
+            publisher (str): Name of the publisher.
+            update_strategy_registry_poll_interval (str, optional): Time interval between checks of the latest
+                catalog_source version.
+        """
+        super().__init__(**kwargs)
         self.source_type = source_type
         self.image = image
         self.display_name = display_name
@@ -40,6 +38,13 @@ class CatalogSource(NamespacedResource):
     def to_dict(self):
         super().to_dict()
         if not self.yaml_file:
+            if not all(
+                [self.source_type, self.image, self.display_name, self.publisher]
+            ):
+                raise ValueError(
+                    "Passing yaml_file or all parameters 'source_type', 'image',"
+                    " 'display_name' and 'publisher' is required."
+                )
             self.res.update(
                 {
                     "spec": {
@@ -50,7 +55,6 @@ class CatalogSource(NamespacedResource):
                     }
                 }
             )
-
             if self.update_strategy_registry_poll_interval:
                 self.res["spec"].update(
                     {
