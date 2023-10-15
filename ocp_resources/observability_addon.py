baa8c57c24c7bdcef38b1fa54f09555b5c3c3e38
Omri Bar-Haim <obarhaim@redhat.com>
2023-10-15 17:41:37 +0300

    adding status to observability-addon

--- ocp_resources/observability_addon.py
+++ ocp_resources/observability_addon.py
@@ -6,4 +6,7 @@ class ObservabilityAddon(NamespacedResource):
     https://github.com/stolostron/multicluster-observability-operator/blob/main/docs/MultiClusterObservability-CRD.md
     """
 
+    class Status(NamespacedResource.Status):
+        AVAILABLE = "Available"
+
     api_group = NamespacedResource.ApiGroup.OBSERVABILITY_ADDON_IO
