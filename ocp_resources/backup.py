6af8fabbbb089de9219a035c38df623c048cc3a4
Dalia Frank <dafrank@dafrank-thinkpadx1nanogen2.remote.csb>
2023-12-11 18:59:03 +0200

    Add data mover params to backup

--- ocp_resources/backup.py
+++ ocp_resources/backup.py
@@ -11,7 +11,7 @@ class Backup(NamespacedResource):
 
     api_group = NamespacedResource.ApiGroup.VELERO_IO
 
-    def __init__(self, included_namespaces=None, excluded_resources=None, **kwargs):
+    def __init__(self, included_namespaces=None, excluded_resources=None, snapshot_move_data=False, storage_location=None, **kwargs):
         """
         Args:
             included_namespaces (list, optional): Namespaces to include in the backup.
@@ -22,7 +22,8 @@ class Backup(NamespacedResource):
         super().__init__(**kwargs)
         self.included_namespaces = included_namespaces
         self.excluded_resources = excluded_resources
-
+        self.snapshot_move_data = snapshot_move_data
+        self.storage_location = storage_location
     def to_dict(self):
         super().to_dict()
         if not self.yaml_file:
@@ -31,5 +32,10 @@ class Backup(NamespacedResource):
                 spec_dict.update({"includedNamespaces": self.included_namespaces})
             if self.excluded_resources:
                 spec_dict.update({"excludedResources": self.excluded_resources})
+            if snapshot_move_data:
+                spec_dict.update({"snapshotMoveData": self.snapshot_move_data})
+            if storage_location:
+                spec_dict.update({"storageLocation": self.storage_location})
             if spec_dict:
                 self.res.update({"spec": spec_dict})
+
