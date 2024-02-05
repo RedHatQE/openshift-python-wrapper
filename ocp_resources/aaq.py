7e6c6ab9baff8b342c5567ffda4556d68e49793a
dbasunag <dbasunag@redhat.com>
2024-02-05 07:18:40 -0500

    move todo to the right place

--- ocp_resources/aaq.py
+++ ocp_resources/aaq.py
@@ -1,6 +1,5 @@
 # API reference: https://github.com/kubevirt/application-aware-quota/blob/main/README.md
 # TODO: update API reference when OCP doc is available
-# TODO: Add missing code e.g. to_dict()
 
 from ocp_resources.resource import Resource
 
@@ -11,3 +10,6 @@ class AAQ(Resource):
     """
 
     api_group = Resource.ApiGroup.AAQ_KUBEVIRT_IO
+
+
+# TODO: Add missing code e.g. to_dict()
