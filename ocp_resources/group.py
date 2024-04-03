dc70be82814f5d0e682b29fb3ae7c3d5f2f5078f
Ruth Netser <rnetser@redhat.com>
2024-04-03 19:33:45 +0300

    Add Group resource (#1761)

---
+++ ocp_resources/group.py
@@ -0,0 +1,18 @@
+from ocp_resources.resource import Resource
+
+
+class Group(Resource):
+    api_group = Resource.ApiGroup.USER_OPENSHIFT_IO
+
+    def __init__(
+        self,
+        users=None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.users = users
+
+    def to_dict(self):
+        super().to_dict()
+        if (not self.yaml_file) and self.users:
+            self.res["users"] = self.users
