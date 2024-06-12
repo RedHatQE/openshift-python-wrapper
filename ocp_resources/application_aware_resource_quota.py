dc8811f91fe49f910190fffe464c206c7af4e855
dshchedr <dshchedr@redhat.com>
2024-06-12 13:45:48 -0700

    Fix for typings in ARQ resources
    Signed-off-by: dshchedr <dshchedr@redhat.com>

--- ocp_resources/application_aware_resource_quota.py
+++ ocp_resources/application_aware_resource_quota.py
@@ -1,5 +1,6 @@
 # API reference: https://github.com/kubevirt/application-aware-quota/tree/main/staging/src/kubevirt.io/application-aware-quota-api/pkg/apis/core/v1alpha1
 # TODO: update API reference when OCP doc is available
+from __future__ import annotations
 from typing import Dict, Any, List
 
 from ocp_resources.resource import MissingRequiredArgumentError, NamespacedResource
