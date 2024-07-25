4939a7c0684e44d95ff9094613c27cdb1af6a1b1
aaguirre <aaguirre@redhat.com>
2024-07-25 11:44:02 +0200

    Fix docstring

--- ocp_resources/dsc_initialization.py
+++ ocp_resources/dsc_initialization.py
@@ -1,10 +1,10 @@
 # Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
 
 from typing import Any, Dict, Optional
-from ocp_resources.resource import NamespacedResource
+from ocp_resources.resource import Resource
 
 
-class DSCInitialization(NamespacedResource):
+class DSCInitialization(Resource):
     """
     DSCInitialization is the Schema for the dscinitializations API.
 
@@ -24,11 +24,62 @@ class DSCInitialization(NamespacedResource):
     ) -> None:
         """
         Args:
-            applications_namespace(str): Namespace for applications to be installed.
-            dev_flags(Dict[Any, Any]): Internal development field to test customizations.
-            monitoring(Dict[Any, Any]): Enable monitoring on specified namespace.
-            service_mesh(Dict[Any, Any]): Configures Service Mesh as networking layer for Data Science Clusters components.
-            trusted_cabundle(Dict[Any, Any]): Configures TrustedCABundle ConfigMap.
+            applications_namespace(str): Namespace for applications to be installed, non-configurable, default to
+               "opendatahub"
+
+            dev_flags(Dict[Any, Any]): Internal development useful field to test customizations. This is not
+               recommended to be used in production environment.
+
+              FIELDS:
+                 logmode	<string>
+
+                     manifestsUri	<string>
+                       Custom manifests uri for odh-manifests
+
+            monitoring(Dict[Any, Any]): Enable monitoring on specified namespace
+
+              FIELDS:
+                 managementState	<string>
+                   Set to one of the following values: - "Managed" : the operator is actively
+                   managing the component and trying to keep it active. It will only upgrade
+                   the component if it is safe to do so. - "Removed" : the operator is
+                   actively managing the component and will not install it, or if it is
+                   installed, the operator will try to remove it.
+
+                     namespace	<string>
+                       Namespace for monitoring if it is enabled
+
+            service_mesh(Dict[Any, Any]): Configures Service Mesh as networking layer for Data Science Clusters
+               components. The Service Mesh is a mandatory prerequisite for single model
+               serving (KServe) and you should review this configuration if you are
+               planning to use KServe. For other components, it enhances user experience;
+               e.g. it provides unified authentication giving a Single Sign On experience.
+
+              FIELDS:
+                 auth	<Object>
+                   Auth holds configuration of authentication and authorization services used
+                   by Service Mesh in Opendatahub.
+
+                     controlPlane	<Object>
+                       ControlPlane holds configuration of Service Mesh used by Opendatahub.
+
+                         managementState	<string>
+
+            trusted_cabundle(Dict[Any, Any]): When set to `Managed`, adds odh-trusted-ca-bundle Configmap to all
+               namespaces that includes cluster-wide Trusted CA Bundle in
+               .data["ca-bundle.crt"]. Additionally, this fields allows admins to add
+               custom CA bundles to the configmap using the .CustomCABundle field.
+
+              FIELDS:
+                 customCABundle	<string> -required-
+                   A custom CA bundle that will be available for all components in the Data
+                   Science Cluster(DSC). This bundle will be stored in odh-trusted-ca-bundle
+                   ConfigMap .data.odh-ca-bundle.crt .
+
+                     managementState	<string> -required-
+                       managementState indicates whether and how the operator should manage
+                       customized CA bundle
+
         """
         super().__init__(**kwargs)
 
