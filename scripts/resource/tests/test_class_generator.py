3b4c8f24f04e671df7f63e52e4e3105cca935ff7
Meni Yakove <myakove@gmail.com>
2024-07-26 20:50:32 +0300

    Use split, not re. add some tests

---
+++ scripts/resource/tests/test_class_generator.py
@@ -0,0 +1,23 @@
+from scripts.resource.class_generator import parse_explain
+from deepdiff import DeepDiff
+import pytest
+from scripts.resource.tests.manifests.results import DATASCIENCECLUSTER, DEPLOYMENT, POD
+
+MANIFESTS_PATH: str = "scripts/resource/tests/manifests"
+
+
+@pytest.mark.parametrize(
+    "explain_file, result_dict",
+    (
+        ("DataScienceCluster.explain", DATASCIENCECLUSTER),
+        ("Deployment.explain", DEPLOYMENT),
+        ("Pod.explain", POD),
+    ),
+)
+def test_parse_explain(explain_file, result_dict):
+    output: str = ""
+    with open(f"{MANIFESTS_PATH}/{explain_file}") as fd:
+        output = fd.read()
+
+    res = parse_explain(api_link="https://test.explain", output=output, namespaced=True)
+    assert not DeepDiff(res, result_dict)
