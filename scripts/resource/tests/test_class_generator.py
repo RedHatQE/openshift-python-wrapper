4f49c7be9e5077c477304bb769de9dda755c3d57
rnetser <rnetser@redhat.com>
2024-07-29 21:37:23 +0300

    remove assert msg

--- scripts/resource/tests/test_class_generator.py
+++ scripts/resource/tests/test_class_generator.py
@@ -48,22 +48,22 @@ def test_parse_explain(tmpdir_factory, kind, debug_file, result_file):
 @pytest.mark.parametrize(
     "camel_case_str, expected",
     [
-        pytest.param("Title", "title", id="title-word"),
-        pytest.param("tolerations", "tolerations", id="lowercase-word"),
-        pytest.param("UPPERCASEWORD", "uppercaseword", id="uppercase-word"),
-        pytest.param("ipFamilies", "ip_families", id="combined-basic-with-two-words"),
+        pytest.param("Title", "title", id="title_word"),
+        pytest.param("tolerations", "tolerations", id="lowercase_word"),
+        pytest.param("UPPERCASEWORD", "uppercaseword", id="uppercase_word"),
+        pytest.param("ipFamilies", "ip_families", id="combined_basic_with_two_words"),
         pytest.param(
             "allocateLoadBalancerNodePorts",
             "allocate_load_balancer_node_ports",
-            id="combined-basic-with-mutliple-words",
+            id="combined_basic_with_multiple_words",
         ),
-        pytest.param("XMLHttpRequest", "xml_http_request", id="combined-uppercase-word-is-first"),
+        pytest.param("XMLHttpRequest", "xml_http_request", id="combined_uppercase_word_is_first"),
         pytest.param(
-            "additionalCORSAllowedOS", "additional_cors_allowed_os", id="combined-uppercase-word-in-the-middle"
+            "additionalCORSAllowedOS", "additional_cors_allowed_os", id="combined_uppercase_word_in_the_middle"
         ),
-        pytest.param("hostIPC", "host_ipc", id="combined-uppercase-word-is-last"),
-        pytest.param("clusterIPs", "cluster_ips", id="combined-uppercase-word-is-last-ends-with-lowercase"),
+        pytest.param("hostIPC", "host_ipc", id="combined_uppercase_word_is_last"),
+        pytest.param("clusterIPs", "cluster_ips", id="combined_uppercase_word_is_last_ends_with_lowercase"),
     ],
 )
-def test_convert_camel_case_to_snake_case(request, camel_case_str, expected):
-    assert convert_camel_case_to_snake_case(string_=camel_case_str) == expected, f"Failed {request.node.callspec.id}"
+def test_convert_camel_case_to_snake_case(camel_case_str, expected):
+    assert convert_camel_case_to_snake_case(string_=camel_case_str) == expected
