d10e880943f25e979577ef521bc41a76f1eb8b5b
Meni Yakove <myakove@gmail.com>
2024-07-29 12:25:36 +0300

    Update tox configuration to run tests based on pull request parent branch

--- tests/test_validate_resources.py
+++ tests/test_validate_resources.py
@@ -4,20 +4,21 @@
 import ast
 import json
 import os
+from typing import Any, Dict, Generator, List, Tuple
 
 import pytest
 
 from ocp_resources.resource import Resource  # noqa
 
 
-def _api_group_name(api_value):
+def _api_group_name(api_value: str) -> str:
     try:
         return eval(f"Resource.ApiGroup.{api_value}")
     except AttributeError:
         return api_value
 
 
-def _api_group_dict(resource_dict, api_group_name):
+def _api_group_dict(resource_dict: Dict[str, Any], api_group_name: str) -> Dict[str, Any]:
     resource_dict_api_group = resource_dict["api_group"]
     api_group = resource_dict_api_group.get(api_group_name)
     if not api_group:
@@ -29,7 +30,7 @@ def _api_group_dict(resource_dict, api_group_name):
     return api_group
 
 
-def _process_api_type(api_type, api_value, resource_dict, cls):
+def _process_api_type(api_type: str, api_value: str, resource_dict, cls: ast.ClassDef) -> List[str]:
     if api_type == "api_group":
         return _get_api_group(
             api_value=api_value,
@@ -47,8 +48,9 @@ def _process_api_type(api_type, api_value, resource_dict, cls):
     return [f"Resource {cls.name} must have api_group or api_version"]
 
 
-def _get_api_group_and_version(bodies):
+def _get_api_group_and_version(bodies: List[Any]) -> Tuple[str, str]:
     for targets in bodies:
+        targets: ast.AnnAssign | ast.Attribute
         if isinstance(targets, ast.AnnAssign):
             api_type = targets.target.id
         else:
@@ -56,8 +58,10 @@ def _get_api_group_and_version(bodies):
 
         return api_type, getattr(targets.value, "attr", getattr(targets.value, "value", None))
 
+    return "", ""
 
-def _get_namespaced(cls, resource_dict, api_value):
+
+def _get_namespaced(cls: ast.ClassDef, resource_dict: Dict[str, Any], api_value: str) -> List[str]:
     errors = []
     for base in getattr(cls, "bases", []):
         api_group_name = _api_group_name(api_value=api_value)
@@ -73,7 +77,7 @@ def _get_namespaced(cls, resource_dict, api_value):
     return errors
 
 
-def _get_api_group(api_value, cls, resource_dict):
+def _get_api_group(api_value: str, cls: ast.ClassDef, resource_dict: Dict[str, Any]) -> List[str]:
     errors = []
     api_group_name = _api_group_name(api_value=api_value)
 
@@ -82,7 +86,7 @@ def _get_api_group(api_value, cls, resource_dict):
     return errors
 
 
-def _get_api_version(api_value, cls, resource_dict):
+def _get_api_version(api_value: str, cls: ast.ClassDef, resource_dict: Dict[str, Any]) -> List[str]:
     errors = []
     api_group_name = _api_group_name(api_value=api_value)
     api_group = _api_group_dict(resource_dict=resource_dict, api_group_name=api_group_name)
@@ -95,23 +99,24 @@ def _get_api_version(api_value, cls, resource_dict):
     return errors
 
 
-def _resource_file():
+def _resource_file() -> Generator[str, None, None]:
     ocp_resources_exclude_files = ["resource.py", "utils.py", "__init__.py"]
-    for root, dirs, files in os.walk("ocp_resources"):
+    for root, _, files in os.walk("ocp_resources"):
         for _file in files:
             if _file in ocp_resources_exclude_files or not _file.endswith(".py"):
                 continue
+
             yield os.path.join(root, _file)
 
 
 @pytest.fixture()
-def resources_definitions():
+def resources_definitions() -> Generator[Dict[str, Any], None, None]:
     with open("tests/scripts/resources_definitions.json") as fd:
         yield json.load(fd)
 
 
 @pytest.fixture()
-def resources_definitions_errors(resources_definitions):
+def resources_definitions_errors(resources_definitions: Dict[str, Any]) -> List[str]:
     errors = []
     for _file in _resource_file():
         with open(_file, "r") as fd:
