6e2290e2a85def866d24c7d034207e65c4dcf231
Meni Yakove <myakove@gmail.com>
2024-08-01 12:38:32 +0300

    Update Pod, aaq, mtq, oauth resources

--- ocp_resources/mtq.py
+++ ocp_resources/mtq.py
@@ -1,12 +1,110 @@
-# API reference: https://github.com/kubevirt/managed-tenant-quota#readme
-# TODO: update API reference when OCP doc is available
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
 
+from typing import Any, Dict, Optional
 from ocp_resources.resource import Resource
 
 
 class MTQ(Resource):
     """
-    MTQ object.
+    MTQ is the MTQ Operator CRD
     """
 
-    api_group = Resource.ApiGroup.MTQ_KUBEVIRT_IO
+    api_group: str = Resource.ApiGroup.MTQ_KUBEVIRT_IO
+
+    def __init__(
+        self,
+        cert_config: Optional[Dict[str, Any]] = None,
+        image_pull_policy: Optional[str] = "",
+        infra: Optional[Dict[str, Any]] = None,
+        priority_class: Optional[str] = "",
+        workload: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            cert_config(Dict[Any, Any]): certificate configuration
+
+              FIELDS:
+                ca	<Object>
+                  CA configuration CA certs are kept in the CA bundle as long as they are
+                  valid
+
+                server	<Object>
+                  Server configuration Certs are rotated and discarded
+
+            image_pull_policy(str): PullPolicy describes a policy for if/when to pull a container image
+
+            infra(Dict[Any, Any]): Rules on which nodes MTQ infrastructure pods will be scheduled
+
+              FIELDS:
+                affinity	<Object>
+                  affinity enables pod affinity/anti-affinity placement expanding the types of
+                  constraints that can be expressed with nodeSelector. affinity is going to be
+                  applied to the relevant kind of pods in parallel with nodeSelector See
+                  https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#affinity-and-anti-affinity
+
+                nodeSelector	<map[string]string>
+                  nodeSelector is the node selector applied to the relevant kind of pods It
+                  specifies a map of key-value pairs: for the pod to be eligible to run on a
+                  node, the node must have each of the indicated key-value pairs as labels (it
+                  can have additional labels as well). See
+                  https://kubernetes.io/docs/concepts/configuration/assign-pod-node/#nodeselector
+
+                tolerations	<[]Object>
+                  tolerations is a list of tolerations applied to the relevant kind of pods
+                  See https://kubernetes.io/docs/concepts/configuration/taint-and-toleration/
+                  for more info. These are additional tolerations other than default ones.
+
+            priority_class(str): PriorityClass of the MTQ control plane
+
+            workload(Dict[Any, Any]): Restrict on which nodes MTQ workload pods will be scheduled
+
+              FIELDS:
+                affinity	<Object>
+                  affinity enables pod affinity/anti-affinity placement expanding the types of
+                  constraints that can be expressed with nodeSelector. affinity is going to be
+                  applied to the relevant kind of pods in parallel with nodeSelector See
+                  https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#affinity-and-anti-affinity
+
+                nodeSelector	<map[string]string>
+                  nodeSelector is the node selector applied to the relevant kind of pods It
+                  specifies a map of key-value pairs: for the pod to be eligible to run on a
+                  node, the node must have each of the indicated key-value pairs as labels (it
+                  can have additional labels as well). See
+                  https://kubernetes.io/docs/concepts/configuration/assign-pod-node/#nodeselector
+
+                tolerations	<[]Object>
+                  tolerations is a list of tolerations applied to the relevant kind of pods
+                  See https://kubernetes.io/docs/concepts/configuration/taint-and-toleration/
+                  for more info. These are additional tolerations other than default ones.
+
+        """
+        super().__init__(**kwargs)
+
+        self.cert_config = cert_config
+        self.image_pull_policy = image_pull_policy
+        self.infra = infra
+        self.priority_class = priority_class
+        self.workload = workload
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.yaml_file:
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            if self.cert_config:
+                _spec["certConfig"] = self.cert_config
+
+            if self.image_pull_policy:
+                _spec["imagePullPolicy"] = self.image_pull_policy
+
+            if self.infra:
+                _spec["infra"] = self.infra
+
+            if self.priority_class:
+                _spec["priorityClass"] = self.priority_class
+
+            if self.workload:
+                _spec["workload"] = self.workload
--- ocp_resources/oauth.py
+++ ocp_resources/oauth.py
@@ -1,9 +1,150 @@
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+from typing import Any, Dict, List, Optional
 from ocp_resources.resource import Resource
 
 
 class OAuth(Resource):
     """
-    OAuth object.
+    OAuth holds cluster-wide information about OAuth.  The canonical name is
+    `cluster`. It is used to configure the integrated OAuth server. This
+    configuration is only honored when the top level Authentication config has
+    type set to IntegratedOAuth.
+     Compatibility level 1: Stable within a major release for a minimum of 12
+    months or 3 minor releases (whichever is longer).
     """
 
-    api_group = Resource.ApiGroup.CONFIG_OPENSHIFT_IO
+    api_group: str = Resource.ApiGroup.CONFIG_OPENSHIFT_IO
+
+    def __init__(
+        self,
+        identity_providers: Optional[List[Any]] = None,
+        templates: Optional[Dict[str, Any]] = None,
+        token_config: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            identity_providers(List[Any]): identityProviders is an ordered list of ways for a user to identify
+              themselves. When this list is empty, no identities are provisioned for
+              users.
+              IdentityProvider provides identities for users authenticating using
+              credentials
+
+              FIELDS:
+                basicAuth	<Object>
+                  basicAuth contains configuration options for the BasicAuth IdP
+
+                github	<Object>
+                  github enables user authentication using GitHub credentials
+
+                gitlab	<Object>
+                  gitlab enables user authentication using GitLab credentials
+
+                google	<Object>
+                  google enables user authentication using Google credentials
+
+                htpasswd	<Object>
+                  htpasswd enables user authentication using an HTPasswd file to validate
+                  credentials
+
+                keystone	<Object>
+                  keystone enables user authentication using keystone password credentials
+
+                ldap	<Object>
+                  ldap enables user authentication using LDAP credentials
+
+                mappingMethod	<string>
+                  mappingMethod determines how identities from this provider are mapped to
+                  users Defaults to "claim"
+
+                name	<string>
+                  name is used to qualify the identities returned by this provider. - It MUST
+                  be unique and not shared by any other identity provider used - It MUST be a
+                  valid path segment: name cannot equal "." or ".." or contain "/" or "%" or
+                  ":" Ref:
+                  https://godoc.org/github.com/openshift/origin/pkg/user/apis/user/validation#ValidateIdentityProviderName
+
+                openID	<Object>
+                  openID enables user authentication using OpenID credentials
+
+                requestHeader	<Object>
+                  requestHeader enables user authentication using request header credentials
+
+                type	<string>
+                  type identifies the identity provider type for this entry.
+
+            templates(Dict[Any, Any]): templates allow you to customize pages like the login page.
+
+              FIELDS:
+                error	<Object>
+                  error is the name of a secret that specifies a go template to use to render
+                  error pages during the authentication or grant flow. The key "errors.html"
+                  is used to locate the template data. If specified and the secret or expected
+                  key is not found, the default error page is used. If the specified template
+                  is not valid, the default error page is used. If unspecified, the default
+                  error page is used. The namespace for this secret is openshift-config.
+
+                login	<Object>
+                  login is the name of a secret that specifies a go template to use to render
+                  the login page. The key "login.html" is used to locate the template data. If
+                  specified and the secret or expected key is not found, the default login
+                  page is used. If the specified template is not valid, the default login page
+                  is used. If unspecified, the default login page is used. The namespace for
+                  this secret is openshift-config.
+
+                providerSelection	<Object>
+                  providerSelection is the name of a secret that specifies a go template to
+                  use to render the provider selection page. The key "providers.html" is used
+                  to locate the template data. If specified and the secret or expected key is
+                  not found, the default provider selection page is used. If the specified
+                  template is not valid, the default provider selection page is used. If
+                  unspecified, the default provider selection page is used. The namespace for
+                  this secret is openshift-config.
+
+            token_config(Dict[Any, Any]): tokenConfig contains options for authorization and access tokens
+
+              FIELDS:
+                accessTokenInactivityTimeout	<string>
+                  accessTokenInactivityTimeout defines the token inactivity timeout for tokens
+                  granted by any client. The value represents the maximum amount of time that
+                  can occur between consecutive uses of the token. Tokens become invalid if
+                  they are not used within this temporal window. The user will need to acquire
+                  a new token to regain access once a token times out. Takes valid time
+                  duration string such as "5m", "1.5h" or "2h45m". The minimum allowed value
+                  for duration is 300s (5 minutes). If the timeout is configured per client,
+                  then that value takes precedence. If the timeout value is not specified and
+                  the client does not override the value, then tokens are valid until their
+                  lifetime.
+                   WARNING: existing tokens' timeout will not be affected (lowered) by
+                  changing this value
+
+                accessTokenInactivityTimeoutSeconds	<integer>
+                  accessTokenInactivityTimeoutSeconds - DEPRECATED: setting this field has no
+                  effect.
+
+                accessTokenMaxAgeSeconds	<integer>
+                  accessTokenMaxAgeSeconds defines the maximum age of access tokens
+
+        """
+        super().__init__(**kwargs)
+
+        self.identity_providers = identity_providers
+        self.templates = templates
+        self.token_config = token_config
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.yaml_file:
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            if self.identity_providers:
+                _spec["identityProviders"] = self.identity_providers
+
+            if self.templates:
+                _spec["templates"] = self.templates
+
+            if self.token_config:
+                _spec["tokenConfig"] = self.token_config
