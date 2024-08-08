e6a30633a37e0352ae91fb7c5a6ac430aea2b010
Meni Yakove <myakove@gmail.com>
2024-08-08 11:06:50 +0300

    create and convert resources using class generator script

--- ocp_resources/deployment.py
+++ ocp_resources/deployment.py
@@ -1,15 +1,146 @@
-# -*- coding: utf-8 -*-
-from ocp_resources.constants import PROTOCOL_ERROR_EXCEPTION_DICT, TIMEOUT_4MINUTES
-from ocp_resources.resource import NamespacedResource
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+from typing import Any, Dict, Optional
+
 from timeout_sampler import TimeoutSampler, TimeoutWatch
+from ocp_resources.constants import PROTOCOL_ERROR_EXCEPTION_DICT, TIMEOUT_4MINUTES
+from ocp_resources.resource import NamespacedResource, MissingRequiredArgumentError
 
 
 class Deployment(NamespacedResource):
     """
-    OpenShift Deployment object.
+    Deployment enables declarative updates for Pods and ReplicaSets.
     """
 
-    api_group = NamespacedResource.ApiGroup.APPS
+    api_group: str = NamespacedResource.ApiGroup.APPS
+
+    def __init__(
+        self,
+        min_ready_seconds: Optional[int] = None,
+        paused: Optional[bool] = None,
+        progress_deadline_seconds: Optional[int] = None,
+        replicas: Optional[int] = None,
+        revision_history_limit: Optional[int] = None,
+        selector: Optional[Dict[str, Any]] = None,
+        strategy: Optional[Dict[str, Any]] = None,
+        template: Optional[Dict[str, Any]] = None,
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            min_ready_seconds(int): Minimum number of seconds for which a newly created pod should be ready
+              without any of its container crashing, for it to be considered available.
+              Defaults to 0 (pod will be considered available as soon as it is ready)
+
+            paused(bool): Indicates that the deployment is paused.
+
+            progress_deadline_seconds(int): The maximum time in seconds for a deployment to make progress before it is
+              considered to be failed. The deployment controller will continue to process
+              failed deployments and a condition with a ProgressDeadlineExceeded reason
+              will be surfaced in the deployment status. Note that progress will not be
+              estimated during the time a deployment is paused. Defaults to 600s.
+
+            replicas(int): Number of desired pods. This is a pointer to distinguish between explicit
+              zero and not specified. Defaults to 1.
+
+            revision_history_limit(int): The number of old ReplicaSets to retain to allow rollback. This is a pointer
+              to distinguish between explicit zero and not specified. Defaults to 10.
+
+            selector(Dict[Any, Any]): Label selector for pods. Existing ReplicaSets whose pods are selected by
+              this will be the ones affected by this deployment. It must match the pod
+              template's labels.
+              A label selector is a label query over a set of resources. The result of
+              matchLabels and matchExpressions are ANDed. An empty label selector matches
+              all objects. A null label selector matches no objects.
+
+              FIELDS:
+                matchExpressions	<[]LabelSelectorRequirement>
+                  matchExpressions is a list of label selector requirements. The requirements
+                  are ANDed.
+
+                matchLabels	<map[string]string>
+                  matchLabels is a map of {key,value} pairs. A single {key,value} in the
+                  matchLabels map is equivalent to an element of matchExpressions, whose key
+                  field is "key", the operator is "In", and the values array contains only
+                  "value". The requirements are ANDed.
+
+            strategy(Dict[Any, Any]): The deployment strategy to use to replace existing pods with new ones.
+              DeploymentStrategy describes how to replace existing pods with new ones.
+
+              FIELDS:
+                rollingUpdate	<RollingUpdateDeployment>
+                  Rolling update config params. Present only if DeploymentStrategyType =
+                  RollingUpdate.
+
+                type	<string>
+                  Type of deployment. Can be "Recreate" or "RollingUpdate". Default is
+                  RollingUpdate.
+
+                  Possible enum values:
+                   - `"Recreate"` Kill all existing pods before creating new ones.
+                   - `"RollingUpdate"` Replace the old ReplicaSets by new one using rolling
+                  update i.e gradually scale down the old ReplicaSets and scale up the new
+                  one.
+
+            template(Dict[Any, Any]): Template describes the pods that will be created. The only allowed
+              template.spec.restartPolicy value is "Always".
+              PodTemplateSpec describes the data a pod should have when created from a
+              template
+
+              FIELDS:
+                metadata	<ObjectMeta>
+                  Standard object's metadata. More info:
+                  https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
+
+                spec	<PodSpec>
+                  Specification of the desired behavior of the pod. More info:
+                  https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#spec-and-status
+
+        """
+        super().__init__(**kwargs)
+
+        self.min_ready_seconds = min_ready_seconds
+        self.paused = paused
+        self.progress_deadline_seconds = progress_deadline_seconds
+        self.replicas = replicas
+        self.revision_history_limit = revision_history_limit
+        self.selector = selector
+        self.strategy = strategy
+        self.template = template
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.yaml_file:
+            if not all([
+                self.selector,
+                self.template,
+            ]):
+                raise MissingRequiredArgumentError(argument="selector, template")
+
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            self.res["selector"] = self.selector
+            self.res["template"] = self.template
+
+            if self.min_ready_seconds:
+                _spec["minReadySeconds"] = self.min_ready_seconds
+
+            if self.paused is not None:
+                _spec["paused"] = self.paused
+
+            if self.progress_deadline_seconds:
+                _spec["progressDeadlineSeconds"] = self.progress_deadline_seconds
+
+            if self.replicas:
+                _spec["replicas"] = self.replicas
+
+            if self.revision_history_limit:
+                _spec["revisionHistoryLimit"] = self.revision_history_limit
+
+            if self.strategy:
+                _spec["strategy"] = self.strategy
 
     def scale_replicas(self, replica_count=int):
         """
@@ -27,7 +158,7 @@ class Deployment(NamespacedResource):
         self.logger.info(f"Set deployment replicas: {replica_count}")
         return self.update(resource_dict=self.res)
 
-    def wait_for_replicas(self, deployed=True, timeout=TIMEOUT_4MINUTES):
+    def wait_for_replicas(self, deployed: bool = True, timeout: int = TIMEOUT_4MINUTES):
         """
         Wait until all replicas are updated.
 
--- ocp_resources/scheduler.py
+++ ocp_resources/scheduler.py
@@ -1,5 +1,95 @@
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+from typing import Any, Dict, Optional
 from ocp_resources.resource import Resource
 
 
 class Scheduler(Resource):
-    api_group = Resource.ApiGroup.CONFIG_OPENSHIFT_IO
+    """
+    Scheduler holds cluster-wide config information to run the Kubernetes
+    Scheduler and influence its placement decisions. The canonical name for this
+    config is `cluster`.
+     Compatibility level 1: Stable within a major release for a minimum of 12
+    months or 3 minor releases (whichever is longer).
+    """
+
+    api_group: str = Resource.ApiGroup.CONFIG_OPENSHIFT_IO
+
+    def __init__(
+        self,
+        default_node_selector: Optional[str] = "",
+        masters_schedulable: Optional[bool] = None,
+        policy: Optional[Dict[str, Any]] = None,
+        profile: Optional[str] = "",
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            default_node_selector(str): defaultNodeSelector helps set the cluster-wide default node selector to
+              restrict pod placement to specific nodes. This is applied to the pods
+              created in all namespaces and creates an intersection with any existing
+              nodeSelectors already set on a pod, additionally constraining that pod's
+              selector. For example, defaultNodeSelector: "type=user-node,region=east"
+              would set nodeSelector field in pod spec to "type=user-node,region=east" to
+              all pods created in all namespaces. Namespaces having project-wide node
+              selectors won't be impacted even if this field is set. This adds an
+              annotation section to the namespace. For example, if a new namespace is
+              created with node-selector='type=user-node,region=east', the annotation
+              openshift.io/node-selector: type=user-node,region=east gets added to the
+              project. When the openshift.io/node-selector annotation is set on the
+              project the value is used in preference to the value we are setting for
+              defaultNodeSelector field. For instance, openshift.io/node-selector:
+              "type=user-node,region=west" means that the default of
+              "type=user-node,region=east" set in defaultNodeSelector would not be
+              applied.
+
+            masters_schedulable(bool): MastersSchedulable allows masters nodes to be schedulable. When this flag is
+              turned on, all the master nodes in the cluster will be made schedulable, so
+              that workload pods can run on them. The default value for this field is
+              false, meaning none of the master nodes are schedulable. Important Note:
+              Once the workload pods start running on the master nodes, extreme care must
+              be taken to ensure that cluster-critical control plane components are not
+              impacted. Please turn on this field after doing due diligence.
+
+            policy(Dict[Any, Any]): DEPRECATED: the scheduler Policy API has been deprecated and will be removed
+              in a future release. policy is a reference to a ConfigMap containing
+              scheduler policy which has user specified predicates and priorities. If this
+              ConfigMap is not available scheduler will default to use
+              DefaultAlgorithmProvider. The namespace for this configmap is
+              openshift-config.
+
+              FIELDS:
+                name	<string> -required-
+                  name is the metadata.name of the referenced config map
+
+            profile(str): profile sets which scheduling profile should be set in order to configure
+              scheduling decisions for new pods.
+               Valid values are "LowNodeUtilization", "HighNodeUtilization", "NoScoring"
+              Defaults to "LowNodeUtilization"
+
+        """
+        super().__init__(**kwargs)
+
+        self.default_node_selector = default_node_selector
+        self.masters_schedulable = masters_schedulable
+        self.policy = policy
+        self.profile = profile
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.yaml_file:
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            if self.default_node_selector:
+                _spec["defaultNodeSelector"] = self.default_node_selector
+
+            if self.masters_schedulable is not None:
+                _spec["mastersSchedulable"] = self.masters_schedulable
+
+            if self.policy:
+                _spec["policy"] = self.policy
+
+            if self.profile:
+                _spec["profile"] = self.profile
