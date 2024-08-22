b6ef30a11d94908fd2fe3dc68acf08dfecf2148b
Ruth Netser <rnetser@redhat.com>
2024-08-22 17:01:29 +0300

    class_generator - add `object` as field type (#2073)
    * add object as field type
    
    * update tests
    
    * add missing class in service
    
    * remove in-solidarity

--- ocp_resources/service.py
+++ ocp_resources/service.py
@@ -9,12 +9,12 @@ class Service(NamespacedResource):
     Service is a named abstraction of software service (for example, mysql) consisting of local port (for example 3306) that the proxy listens on, and the selector that determines which pods will answer requests sent through the proxy.
     """
 
+    api_version: str = NamespacedResource.ApiVersion.V1
+
     class Type:
         CLUSTER_IP = "ClusterIP"
         NODE_PORT = "NodePort"
         LOAD_BALANCER = "LoadBalancer"
-
-    api_version: str = NamespacedResource.ApiVersion.V1
 
     def __init__(
         self,
