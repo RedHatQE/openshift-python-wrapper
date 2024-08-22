5beff717e6e1272d5fb367a435ced0118b93cd56
Meni Yakove <myakove@gmail.com>
2024-08-22 20:52:45 +0300

    Fix deprecated msg

--- ocp_resources/project.py
+++ ocp_resources/project.py
@@ -5,8 +5,9 @@ from ocp_resources.project_project_openshift_io import Project, ProjectRequest  
 
 warn(
     f"The module {__name__} is deprecated and will be removed in version 4.17, "
-    "`Project` and `ProjectRequest` should be either imported from `ocp_resources.project_project_openshift_io` or "
-    "`ocp_resources.project_config_openshift_io` depending on the API group.",
+    "`Project` should be either imported from `ocp_resources.project_project_openshift_io` or "
+    "`ocp_resources.project_config_openshift_io` depending on the API group."
+    "`ProjectRequest` should be imported from `ocp_resources.project_project_openshift_io`",
     DeprecationWarning,
     stacklevel=2,
 )
