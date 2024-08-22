eb80ebf98d1de39575c836b70c3b6ec43091067a
rnetser <rnetser@redhat.com>
2024-08-22 16:11:27 +0300

    tests - validate resources use class_generator

--- tests/test_validate_resources.py
+++ tests/test_validate_resources.py
@@ -1,6 +1,7 @@
 # This test uses https://github.com/RedHatQE/openshift-resources-definitions to get resource definitions json file
 # File: resources_definitions.json
 
+from __future__ import annotations
 import ast
 import json
 import os
@@ -13,39 +14,10 @@ from ocp_resources.resource import Resource  # noqa
 
 def _api_group_name(api_value: str) -> str:
     try:
-        return eval(f"Resource.ApiGroup.{api_value}")
+        if api_value:
+            return eval(f"Resource.ApiGroup.{api_value}")
     except AttributeError:
         return api_value
-
-
-def _api_group_dict(resource_dict: Dict[str, Any], api_group_name: str) -> Dict[str, Any]:
-    resource_dict_api_group = resource_dict["api_group"]
-    api_group = resource_dict_api_group.get(api_group_name)
-    if not api_group:
-        try:
-            api_group = resource_dict_api_group["null"]
-        except KeyError:
-            api_group = resource_dict_api_group[[*resource_dict_api_group][0]]
-
-    return api_group
-
-
-def _process_api_type(api_type: str, api_value: str, resource_dict, cls: ast.ClassDef) -> List[str]:
-    if api_type == "api_group":
-        return _get_api_group(
-            api_value=api_value,
-            cls=cls,
-            resource_dict=resource_dict,
-        )
-
-    if api_type == "api_version":
-        return _get_api_version(
-            api_value=api_value,
-            cls=cls,
-            resource_dict=resource_dict,
-        )
-
-    return [f"Resource {cls.name} must have api_group or api_version"]
 
 
 def _get_api_group_and_version(bodies: List[Any]) -> Tuple[str, str]:
@@ -61,19 +33,45 @@ def _get_api_group_and_version(bodies: List[Any]) -> Tuple[str, str]:
     return "", ""
 
 
-def _get_namespaced(cls: ast.ClassDef, resource_dict: Dict[str, Any], api_value: str) -> List[str]:
+def validate_resource(
+    cls: ast.ClassDef,
+    resource_list: List[Dict[str, Any]],
+    api_value: str,
+    api_type: str,
+) -> List[str]:
     errors = []
-    for base in getattr(cls, "bases", []):
-        api_group_name = _api_group_name(api_value=api_value)
-        namespaced = base.id == "NamespacedResource"
-        api_group = _api_group_dict(resource_dict=resource_dict, api_group_name=api_group_name)
-        should_be_namespaced = api_group["namespaced"] == "true"
 
-        if namespaced != should_be_namespaced:
-            errors.append(
-                f"Resource {cls.name} should be "
-                f"{'namespaced' if should_be_namespaced else 'in cluster scope (not namespaced)'}"
-            )
+    if not api_type:
+        errors.append(f"Resource {cls.name} must have api_group or api_version")
+
+    for base in getattr(cls, "bases", []):
+        namespaced = base.id == "NamespacedResource"
+        api_group_name = _api_group_name(api_value=api_value)
+        for resource_dict in resource_list:
+            for group_version_kind in resource_dict["x-kubernetes-group-version-kind"]:
+                if api_type == "api_group":
+                    if (api_group := group_version_kind["group"]) == api_group_name:
+                        # Check if resource is namespaced
+                        if namespaced != resource_dict["namespaced"]:
+                            errors.append(
+                                f"Resource {cls.name} should be "
+                                f"{'namespaced' if namespaced else 'in cluster scope (not namespaced)'}"
+                            )
+
+                        # If resource has api group but class has api version
+                        if api_type == "api_version":
+                            errors.append(
+                                f"Resource {cls.name} have api_version {api_value} but should have api_group = "
+                                f"{api_group.upper()}"
+                            )
+
+                else:
+                    if (resource_api_version := group_version_kind["version"].lower()) != api_value.lower():
+                        errors.append(
+                            f"Resource {cls.name} have api_version {api_value} but should have api_version = "
+                            f"{resource_api_version}"
+                        )
+
     return errors
 
 
@@ -83,19 +81,6 @@ def _get_api_group(api_value: str, cls: ast.ClassDef, resource_dict: Dict[str, A
 
     if api_group_name not in resource_dict["api_group"]:
         errors.append(f"Resource {cls.name} api_group should be {resource_dict['api_group']}. got {api_group_name}")
-    return errors
-
-
-def _get_api_version(api_value: str, cls: ast.ClassDef, resource_dict: Dict[str, Any]) -> List[str]:
-    errors = []
-    api_group_name = _api_group_name(api_value=api_value)
-    api_group = _api_group_dict(resource_dict=resource_dict, api_group_name=api_group_name)
-    if api_value.lower() != api_group["api_version"]:
-        desire_api_group = resource_dict["api_version"].split("/")[0]
-        errors.append(
-            f"Resource {cls.name} have api_version {api_value} but should have api_group = {desire_api_group}"
-        )
-
     return errors
 
 
@@ -111,7 +96,7 @@ def _resource_file() -> Generator[str, None, None]:
 
 @pytest.fixture()
 def resources_definitions() -> Generator[Dict[str, Any], None, None]:
-    with open("tests/scripts/resources_definitions.json") as fd:
+    with open("class_generator/schema/__resources-mappings.json") as fd:
         yield json.load(fd)
 
 
@@ -119,27 +104,31 @@ def resources_definitions() -> Generator[Dict[str, Any], None, None]:
 def resources_definitions_errors(resources_definitions: Dict[str, Any]) -> List[str]:
     errors = []
     for _file in _resource_file():
-        with open(_file, "r") as fd:
-            tree = ast.parse(source=fd.read())
+        with open(_file) as fd:
+            data = fd.read()
+            if data.startswith(
+                "# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md"
+            ):
+                continue
+
+            tree = ast.parse(source=data)
             classes = [cls for cls in tree.body if isinstance(cls, ast.ClassDef)]
             for cls in classes:
-                resource_dict = resources_definitions.get(cls.name)
-                if not resource_dict:
-                    # TODO: Fail and let the user know that 'tests/scripts/resources_definitions.json'
-                    #   need to be updated using 'update_resources_definitions' script
+                resource_list = resources_definitions.get(cls.name.lower())
+                if not resource_list:
                     continue
 
                 bodies = [body_ for body_ in getattr(cls, "body") if isinstance(body_, (ast.Assign, ast.AnnAssign))]
                 api_type, api_value = _get_api_group_and_version(bodies=bodies)
-                errors.extend(_get_namespaced(cls=cls, resource_dict=resource_dict, api_value=api_value))
                 errors.extend(
-                    _process_api_type(
-                        api_type=api_type,
-                        api_value=api_value,
-                        resource_dict=resource_dict,
+                    validate_resource(
                         cls=cls,
+                        resource_list=resource_list,
+                        api_value=api_value,
+                        api_type=api_type,
                     )
                 )
+
     return errors
 
 
