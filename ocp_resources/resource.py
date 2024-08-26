dc55809741d498151a9e96cb3d160f05f89eb3a1
Shahaf Bahar <sbahar@redhat.com>
2024-08-26 11:28:44 +0300

    Add type hint for proxy variable

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -112,7 +112,7 @@ def get_client(
         # If `KUBECONFIG` environment variable is set via code, the `KUBE_CONFIG_DEFAULT_LOCATION` will be None since
         # is populated during import which comes before setting the variable in code.
         config_file = config_file or os.environ.get("KUBECONFIG", "~/.kube/config")
-        proxy = kwargs.pop("proxy", None) or os.environ.get("HTTPS_PROXY") or os.environ.get("HTTP_PROXY")
+        proxy: Optional[str] = kwargs.pop("proxy", None) or os.environ.get("HTTPS_PROXY") or os.environ.get("HTTP_PROXY")
 
         if proxy:
             LOGGER.info(f"Trying to get client using proxy {proxy}")
