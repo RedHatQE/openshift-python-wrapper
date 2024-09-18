a1923472d0af439c3934dc840ef3cc6c6bf52aca
rnetser <rnetser@redhat.com>
2024-09-18 16:26:13 +0300

    smm add description

--- ocp_resources/service_mesh_member.py
+++ ocp_resources/service_mesh_member.py
@@ -19,7 +19,7 @@ class ServiceMeshMember(NamespacedResource):
     ) -> None:
         """
         Args:
-            control_plane_ref(Dict[str, Any]): No field description from API; please add description
+            control_plane_ref(Dict[str, Any]): Dict with `ServiceMeshControlPlane` information.
 
         """
         super().__init__(**kwargs)
