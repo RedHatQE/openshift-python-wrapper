a8012e5df62711ea6f5d90e21e28cb5d0d5001d7
dbasunag <dbasunag@redhat.com>
2024-09-22 08:54:29 -0400

    remove f string

--- ocp_resources/configmap.py
+++ ocp_resources/configmap.py
@@ -2,8 +2,8 @@ from warnings import warn
 from ocp_resources.config_map import ConfigMap  # noqa: F401
 
 warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, `ConfigMap` should be imported from "
-    f"`ocp_resources.config_map`",
+    f"The module {__name__} is deprecated and will be removed in version 4.17, `ConfigMap` should be "
+    "imported from `ocp_resources.config_map`",
     DeprecationWarning,
     stacklevel=2,
 )
