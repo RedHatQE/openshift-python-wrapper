c634e58bb206a6763c5e9855bb420004847577f9
Shahaf Bahar <110476642+sbahar619@users.noreply.github.com>
2024-10-01 11:11:43 +0300

    Add UserDefinedNetwork resource using class generator (#2126)
    * Add UDN resource using class generator
    
    * Revert all schema files
    
    Signed-off-by: Shahaf Bahar <sbahar@redhat.com>
    
    * Correct the grammatical error in the class docstring
    
    * Move constant location
    
    ---------
    
    Signed-off-by: Shahaf Bahar <sbahar@redhat.com>
    Co-authored-by: Meni Yakove <441263+myakove@users.noreply.github.com>

---
+++ ocp_resources/user_defined_network.py
@@ -0,0 +1,68 @@
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+from typing import Any, Dict, Optional
+from ocp_resources.resource import NamespacedResource, MissingRequiredArgumentError
+
+
+class UserDefinedNetwork(NamespacedResource):
+    """
+    UserDefinedNetwork describes network request for a Namespace.
+    """
+
+    api_group: str = NamespacedResource.ApiGroup.K8S_OVN_ORG
+
+    def __init__(
+        self,
+        layer2: Optional[Dict[str, Any]] = None,
+        layer3: Optional[Dict[str, Any]] = None,
+        local_net: Optional[Dict[str, Any]] = None,
+        topology: Optional[str] = "",
+        **kwargs: Any,
+    ) -> None:
+        """
+        Args:
+            layer2 (Dict[str, Any]): Layer2 is the Layer2 topology configuration.
+
+            layer3 (Dict[str, Any]): Layer3 is the Layer3 topology configuration.
+
+            local_net (Dict[str, Any]): LocalNet is the LocalNet topology configuration.
+
+            topology (str): Topology describes network configuration.   Allowed values are
+              "Layer3", "Layer2", "LocalNet". Layer3 topology creates a layer 2
+              segment per node, each with a different subnet. Layer 3 routing is
+              used to interconnect node subnets. Layer2 topology creates one
+              logical switch shared by all nodes. LocalNet topology creates a
+              cluster-wide logical switch connected to a physical network.
+
+        """
+        super().__init__(**kwargs)
+
+        self.layer2 = layer2
+        self.layer3 = layer3
+        self.local_net = local_net
+        self.topology = topology
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.yaml_file:
+            if not all([
+                self.topology,
+            ]):
+                raise MissingRequiredArgumentError(argument="topology")
+
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            _spec["topology"] = self.topology
+
+            if self.layer2:
+                _spec["layer2"] = self.layer2
+
+            if self.layer3:
+                _spec["layer3"] = self.layer3
+
+            if self.local_net:
+                _spec["localNet"] = self.local_net
+
+    # End of generated code
