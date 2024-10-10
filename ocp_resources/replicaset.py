5e04bfd2c26d3913472a5b1891e261d9524f3c81
Meni Yakove <441263+myakove@users.noreply.github.com>
2024-10-10 14:58:09 +0300

    remove deperecated code (#2161)

--- ocp_resources/replicaset.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from warnings import warn
-from ocp_resources.replica_set import ReplicaSet  # noqa: F401
-
-warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, `ReplicaSet` should be imported from `ocp_resources.replica_set`",
-    DeprecationWarning,
-    stacklevel=2,
-)
