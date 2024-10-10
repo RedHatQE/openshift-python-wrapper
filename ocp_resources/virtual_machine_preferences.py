46ae671eeb33ecd8fbc049c3b8ccffe25597159a
Meni Yakove <myakove@gmail.com>
2024-10-10 14:43:36 +0300

    remove deperecated code

--- ocp_resources/virtual_machine_preferences.py
+++
@@ -1,8 +0,0 @@
-from warnings import warn
-from ocp_resources.virtual_machine_preference import VirtualMachinePreference  # noqa: F401
-
-warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, `VirtualMachinePreference` should be imported from `ocp_resources.virtual_machine_preference`",
-    DeprecationWarning,
-    stacklevel=2,
-)
