58e73495aece0424580c4b7f89d246050e73189a
Meni Yakove <441263+myakove@users.noreply.github.com>
2024-10-10 14:58:09 +0300

    remove deperecated code (#2161)

--- ocp_resources/virtual_machine_instance_types.py
+++
@@ -1,8 +0,0 @@
-from warnings import warn
-from ocp_resources.virtual_machine_instancetype import VirtualMachineInstancetype  # noqa: F401
-
-warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, `VirtualMachineInstancetype` should be imported from `ocp_resources.virtual_machine_instancetype`",
-    DeprecationWarning,
-    stacklevel=2,
-)
