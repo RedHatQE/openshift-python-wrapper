4d5dc093b57e7e950ee7c1d573fbe5b3164e3cac
Meni Yakove <441263+myakove@users.noreply.github.com>
2024-10-13 10:45:27 +0300

    Fix modules deprecated warn version (#2163)

--- ocp_resources/image.py
+++ ocp_resources/image.py
@@ -3,7 +3,7 @@ from warnings import warn
 from ocp_resources.image_image_openshift_io import Image  # noqa: F401
 
 warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, "
+    f"The module {__name__} is deprecated and will be removed in version 4.18, "
     "`Image` should be either imported from `ocp_resources.image_image_openshift_io` or "
     "`ocp_resources.image_config_openshift_io` depending on the API group.",
     DeprecationWarning,
--- ocp_resources/nmstate.py
+++ ocp_resources/nmstate.py
@@ -3,7 +3,7 @@ from warnings import warn
 from ocp_resources.nm_state import NMState  # noqa: F401
 
 warn(
-    f"The module {__name__} is deprecated and will be removed in version 4.17, "
+    f"The module {__name__} is deprecated and will be removed in version 4.18, "
     "`NMState` should be imported from `ocp_resources.nm_state`",
     DeprecationWarning,
     stacklevel=2,
