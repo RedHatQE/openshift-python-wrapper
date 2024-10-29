1639f1ad05d6b0c78ebc134c55636aa24abb1ca5
aaguirre <aaguirre@redhat.com>
2024-10-29 11:30:21 +0100

    Fix mypy issues

--- ocp_resources/cluster_service_version.py
+++ ocp_resources/cluster_service_version.py
@@ -10,13 +10,14 @@ class ClusterServiceVersion(NamespacedResource):
     class Status(NamespacedResource.Status):
         INSTALLING = "Installing"
 
-    def get_alm_examples(self) -> List[Dict[str, Any]]:
+    def get_dicts_from_examples(self) -> List[dict[str, Any]]:
         """
         Parse the alm-examples annotation from the CSV instance and return a list of dictionaries.
 
         Returns:
-            List[Dict[str, Any]]: A list of dictionaries, each representing a different kind from alm-examples
+            List[dict[str, Any]]: A list of dictionaries from alm-examples. Returns empty list if
+            alm-examples doesn't exist or is invalid.
         """
 
         examples = self.instance.metadata.annotations.get("alm-examples")
-        return json.loads(examples) if examples else None
+        return json.loads(examples) if examples else []
