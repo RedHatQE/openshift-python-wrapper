d9bfa53cd85454470481ba309645999577045f17
dbasunag <dbasunag@redhat.com>
2024-11-26 14:32:33 -0500

    updates based on reviews

--- tests/test_unittests.py
+++ tests/test_unittests.py
@@ -35,12 +35,13 @@ def vm_spec():
     ],
 )
 def test_replace_key_with_hashed_value(vm_spec, key_name, expected_key):
-    assert (
-        benedict(replace_key_with_hashed_value(resource_dict=vm_spec, key_name=key_name), keypath_separator=">")[
-            expected_key
-        ]
-        == "*******"
-    )
+    result = benedict(replace_key_with_hashed_value(resource_dict=vm_spec, key_name=key_name), keypath_separator=">")
+    assert result[expected_key] == "*******"
+    vm_spec_benedict = benedict(vm_spec, keypath_separator=">")
+    result_benedict = benedict(result, keypath_separator=">")
+    del vm_spec_benedict[expected_key]
+    del result_benedict[expected_key]
+    assert result_benedict == vm_spec_benedict
 
 
 @pytest.mark.parametrize(
@@ -52,6 +53,12 @@ def test_replace_key_with_hashed_value(vm_spec, key_name, expected_key):
         pytest.param(
             {"x": {"y": "z"}}, "a>b", {"x": {"y": "z"}}, id="test_replace_key_with_hashed_value_key_doesnt_exist"
         ),
+        # Malformed key
+        pytest.param(
+            {"x": {"y": "z"}}, "x>y>", {"x": {"y": "z"}}, id="test_replace_key_with_hashed_value_malformed_key"
+        ),
+        # empty key path
+        pytest.param({"x": {"y": "z"}}, "", {"x": {"y": "z"}}, id="test_replace_key_with_hashed_value_empty_key"),
     ],
 )
 def test_replace_key_with_hashed_value_edge_cases(resource, key_name, expected_result):
@@ -66,7 +73,16 @@ def test_replace_key_with_hashed_value_multiple_occurances(vm_spec):
         assert volume["userData"] == "*******"
 
 
-def test_replace_key_with_hashed_value_invalid_inputs():
+@pytest.mark.parametrize(
+    "resource, key_name, exception_type",
+    [
+        pytest.param(None, "a>b", TypeError, id="test_replace_key_with_hashed_value_empty_dict_valid_key"),
+        pytest.param({}, None, TypeError, id="test_replace_key_with_hashed_value_empty_dict_no_key"),
+        pytest.param({}, 123, TypeError, id="test_replace_key_with_hashed_value_empty_dict_invalid_key"),
+        pytest.param("not_a_dict", "a>b", ValueError, id="test_replace_key_with_hashed_value_invalid_dict_valid_key"),
+    ],
+)
+def test_replace_key_with_hashed_value_invalid_inputs(resource, key_name, exception_type):
     """Test that the function handles invalid inputs appropriately."""
-    with pytest.raises(TypeError):
-        replace_key_with_hashed_value(resource_dict=None, key_name="a>b")
+    with pytest.raises(exception_type):
+        replace_key_with_hashed_value(resource_dict=resource, key_name=key_name)
