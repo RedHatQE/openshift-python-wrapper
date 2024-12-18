c706f32bf59b5745e4feb9b99087e2b787bdb510
dbasunag <dbasunag@redhat.com>
2024-12-18 17:32:37 -0500

    add optional retry_params to pass timeout and sleep_time

--- ocp_resources/constants.py
+++ ocp_resources/constants.py
@@ -25,6 +25,7 @@ DEFAULT_CLUSTER_RETRY_EXCEPTIONS: Dict[type[Exception], List[str]] = {
 PROTOCOL_ERROR_EXCEPTION_DICT: Dict[type[Exception], List[str]] = {ProtocolError: []}
 NOT_FOUND_ERROR_EXCEPTION_DICT: Dict[type[Exception], List[str]] = {NotFoundError: []}
 
+TIMEOUT_1SEC: int = 1
 TIMEOUT_5SEC: int = 5
 TIMEOUT_10SEC: int = 10
 TIMEOUT_30SEC: int = 30
@@ -32,3 +33,5 @@ TIMEOUT_1MINUTE: int = 60
 TIMEOUT_2MINUTES: int = 2 * 60
 TIMEOUT_4MINUTES: int = 4 * 60
 TIMEOUT_10MINUTES: int = 10 * 60
+
+RETRY_FLAG = {"timeout": TIMEOUT_30SEC, "sleep_time": TIMEOUT_5SEC}
