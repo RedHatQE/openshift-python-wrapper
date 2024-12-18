4b9ef86dc2706f0b2c583a199ff680128dd26d8a
Meni Yakove <myakove@gmail.com>
2024-12-18 12:48:41 +0200

    remove MTV resource, Move Resource constants to class

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -284,66 +284,6 @@ class Resource(ResourceConstants):
     singular_name: str = ""
     timeout_seconds: int = TIMEOUT_1MINUTE
 
-    # class Status:
-    #     SUCCEEDED: str = "Succeeded"
-    #     FAILED: str = "Failed"
-    #     DELETING: str = "Deleting"
-    #     DEPLOYED: str = "Deployed"
-    #     PENDING: str = "Pending"
-    #     COMPLETED: str = "Completed"
-    #     RUNNING: str = "Running"
-    #     READY: str = "Ready"
-    #     TERMINATING: str = "Terminating"
-    #     ERROR: str = "Error"
-    #     COMPLETE: str = "Complete"
-    #     DEPLOYING: str = "Deploying"
-    #     SCHEDULING_DISABLED = "Ready,SchedulingDisabled"
-    #     CRASH_LOOPBACK_OFF = "CrashLoopBackOff"
-    #     IMAGE_PULL_BACK_OFF = "ImagePullBackOff"
-    #     ERR_IMAGE_PULL = "ErrImagePull"
-    #     ACTIVE = "Active"
-    #
-    # class Condition:
-    #     UPGRADEABLE: str = "Upgradeable"
-    #     AVAILABLE: str = "Available"
-    #     DEGRADED: str = "Degraded"
-    #     PROGRESSING: str = "Progressing"
-    #     CREATED: str = "Created"
-    #     RECONCILE_COMPLETE: str = "ReconcileComplete"
-    #     READY: str = "Ready"
-    #     FAILING: str = "Failing"
-    #
-    #     class Status:
-    #         TRUE: str = "True"
-    #         FALSE: str = "False"
-    #         UNKNOWN: str = "Unknown"
-    #
-    #     class Phase:
-    #         INSTALL_READY: str = "InstallReady"
-    #         SUCCEEDED: str = "Succeeded"
-    #
-    #     class Reason:
-    #         ALL_REQUIREMENTS_MET: str = "AllRequirementsMet"
-    #         INSTALL_SUCCEEDED: str = "InstallSucceeded"
-    #         NETWORK_ATTACHMENT_DEFINITION_READY: str = "NetworkAttachmentDefinitionReady"
-    #         SYNC_ERROR: str = "SyncError"
-    #
-    #     class Type:
-    #         NETWORK_READY: str = "NetworkReady"
-    #         SUCCESSFUL: str = "Successful"
-    #         RUNNING: str = "Running"
-    #
-    # class Type:
-    #     CLUSTER_IP = "ClusterIP"
-    #     NODE_PORT = "NodePort"
-    #     LOAD_BALANCER = "LoadBalancer"
-    #
-    # class Interface:
-    #     class State:
-    #         UP: str = "up"
-    #         DOWN: str = "down"
-    #         ABSENT: str = "absent"
-
     class ApiGroup:
         AAQ_KUBEVIRT_IO: str = "aaq.kubevirt.io"
         ADMISSIONREGISTRATION_K8S_IO: str = "admissionregistration.k8s.io"
