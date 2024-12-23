4dcafeb75828b29ffecb6e013f4d2c77e74ce945
Ahmad Hafe <ahafe@redhat.com>
2024-12-23 20:01:44 +0200

    remove garbage collector dead code

--- ocp_resources/datavolume.py
+++ ocp_resources/datavolume.py
@@ -8,6 +8,9 @@ from ocp_resources.utils.constants import (
 from ocp_resources.persistent_volume_claim import PersistentVolumeClaim
 from ocp_resources.resource import NamespacedResource, Resource
 from timeout_sampler import TimeoutExpiredError, TimeoutSampler
+from warnings import warn
+
+gc_deprecatio_warning = ("garbage collector is removed in version v1.62 and going to be deprecated", DeprecationWarning)
 
 
 class DataVolume(NamespacedResource):
@@ -281,7 +284,7 @@ class DataVolume(NamespacedResource):
         Args:
             timeout (int):  Time to wait for the DataVolume to succeed.
             failure_timeout (int): Time to wait for the DataVolume to have not Pending/None status
-            dv_garbage_collection_enabled (bool, default: False): if True, expect that DV will disappear after success
+            dv_garbage_collection_enabled (bool, default: False): garbage collector is removed in version v1.62 and going to be deprecated
             stop_status_func (function): function that is called inside the TimeoutSampler
                 if it returns True - stop the Sampler and raise TimeoutExpiredError
                 Example:
@@ -307,6 +310,8 @@ class DataVolume(NamespacedResource):
                 wait_timeout=timeout,
                 func=lambda: self.exists,
             ):
+                if dv_garbage_collection_enabled is not None:
+                    warn(gc_deprecatio_warning)
                 # DV reach success if the status is Succeeded, or if DV garbage collection enabled and the DV does not exist
                 if sample and sample.get("status", {}).get("phase") == self.Status.SUCCEEDED:
                     break
@@ -341,6 +346,7 @@ class DataVolume(NamespacedResource):
         """
         # if garbage collector is enabled, DV will be deleted after success
         if self.exists:
+            warn(gc_deprecatio_warning)
             return super().delete(wait=wait, timeout=timeout, body=body)
         else:
             return self.pvc.delete(wait=wait, timeout=timeout, body=body)
