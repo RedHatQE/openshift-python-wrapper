36581e174d43890080bf37e077e0aba283cb6b80
Jenia Peimer <86722603+jpeimer@users.noreply.github.com>
2025-04-17 13:00:34 +0300

    feat: add MigPlan and MigCluster resources, update class-generator schema (#2382)

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -357,6 +357,7 @@ class Resource(ResourceConstants):
         MAISTRA_IO: str = "maistra.io"
         METALLB_IO: str = "metallb.io"
         METRICS_K8S_IO: str = "metrics.k8s.io"
+        MIGRATION_OPENSHIFT_IO: str = "migration.openshift.io"
         MIGRATIONS_KUBEVIRT_IO: str = "migrations.kubevirt.io"
         MODELREGISTRY_OPENDATAHUB_IO: str = "modelregistry.opendatahub.io"
         MONITORING_COREOS_COM: str = "monitoring.coreos.com"
