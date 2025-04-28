27e7c5814667cadc5016bee3c57824812be0c077
Jiri Daněk <jdanek@redhat.com>
2025-04-28 22:28:11 +0200

    feat: add SelfSubjectReview resource (#2389)

---
+++ ocp_resources/self_subject_review.py
@@ -0,0 +1,22 @@
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+from __future__ import annotations
+
+from typing import Any
+from ocp_resources.resource import Resource
+
+
+class SelfSubjectReview(Resource):
+    """
+    SelfSubjectReview contains the user information that the kube-apiserver has about the user making this request. When using impersonation, users will receive the user info of the user being impersonated.  If impersonation or request header authentication is used, any extra keys will have their case ignored and returned as lowercase.
+    """
+
+    api_group: str = Resource.ApiGroup.AUTHENTICATION_K8S_IO
+
+    def __init__(
+        self,
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(**kwargs)
+
+    # End of generated code
