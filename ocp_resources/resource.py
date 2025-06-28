6e8dd10670abf3bbeeb4cf8f453645d406e1cf0f
Meni Yakove <441263+myakove@users.noreply.github.com>
2025-06-28 18:50:32 +0300

    feat: Add snapshot resource (#2422)

--- ocp_resources/resource.py
+++ ocp_resources/resource.py
@@ -443,6 +443,7 @@ class Resource(ResourceConstants):
         APIREGISTRATION_K8S_IO: str = "apiregistration.k8s.io"
         APP_KUBERNETES_IO: str = "app.kubernetes.io"
         APPS: str = "apps"
+        APPSTUDIO_REDHAT_COM: str = "appstudio.redhat.com"
         AUTHENTICATION_K8S_IO: str = "authentication.k8s.io"
         BATCH: str = "batch"
         BITNAMI_COM: str = "bitnami.com"
