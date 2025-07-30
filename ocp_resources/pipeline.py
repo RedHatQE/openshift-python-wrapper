bfd8963d5f35b2a4a14155c52dbdc435f884a0f6
Geetika Kapoor <13978799+geetikakay@users.noreply.github.com>
2025-07-30 09:59:19 +0100

    fix: Auto-generate tekton resources (#2476)

--- ocp_resources/pipeline.py
+++ ocp_resources/pipeline.py
@@ -1,45 +1,91 @@
-# API reference: https://tekton.dev/docs/pipelines/pipelines/
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
 
-from ocp_resources.resource import MissingRequiredArgumentError, NamespacedResource
+
+from typing import Any
+from ocp_resources.resource import NamespacedResource
 
 
 class Pipeline(NamespacedResource):
-    api_group = NamespacedResource.ApiGroup.TEKTON_DEV
+    """
+        Pipeline describes a list of Tasks to execute. It expresses how outputs
+    of tasks feed into inputs of subsequent tasks.
+    """
+
+    api_group: str = NamespacedResource.ApiGroup.TEKTON_DEV
 
     def __init__(
         self,
-        tasks=None,
-        params=None,
-        final_parallel_tasks=None,
-        **kwargs,
-    ):
-        """
+        description: str | None = None,
+        display_name: str | None = None,
+        finally_: list[Any] | None = None,
+        params: list[Any] | None = None,
+        results: list[Any] | None = None,
+        tasks: list[Any] | None = None,
+        workspaces: list[Any] | None = None,
+        **kwargs: Any,
+    ) -> None:
+        r"""
         Args:
-            tasks (str, optional): actions to perform in pipeline
-            params (dict, optional):  params to support pipelines.
-            params can be set/changed based on tasks.
-            example: 'spec': {'params': [{'name': 'sourceTemplateName','type': 'string','default':'openshift'},
-            {'name': 'sourceTemplateNamespace', 'type':'string', 'description': 'Namespace pf template'}]}
-            final_parallel_tasks (list, optional):  a list of one or more to be executed in parallel after all other
-            tasks have completed in parallel.
-            spec section can't be empty. It requires at least one optional field.
+            description (str): Description is a user-facing description of the pipeline that may be
+              used to populate a UI.
+
+            display_name (str): DisplayName is a user-facing name of the pipeline that may be used to
+              populate a UI.
+
+            finally_ (list[Any]): Finally declares the list of Tasks that execute just before leaving
+              the Pipeline i.e. either after all Tasks are finished executing
+              successfully or after a failure which would result in ending the
+              Pipeline
+
+                Note: Parameter renamed from &#39;finally&#39; to avoid Python keyword conflict.
+            params (list[Any]): Params declares a list of input parameters that must be supplied when
+              this Pipeline is run.
+
+            results (list[Any]): Results are values that this pipeline can output once run
+
+            tasks (list[Any]): Tasks declares the graph of Tasks that execute when this Pipeline is
+              run.
+
+            workspaces (list[Any]): Workspaces declares a set of named workspaces that are expected to be
+              provided by a PipelineRun.
+
         """
         super().__init__(**kwargs)
-        # TODO: Add a check for tasks when bug https://issues.redhat.com/browse/SRVKP-3019 is resolved.
-        self.tasks = tasks
+
+        self.description = description
+        self.display_name = display_name
+        self.finally_ = finally_
         self.params = params
-        self.final_parallel_tasks = final_parallel_tasks
+        self.results = results
+        self.tasks = tasks
+        self.workspaces = workspaces
 
     def to_dict(self) -> None:
         super().to_dict()
-        if not self.kind_dict and not self.yaml_file:
-            if not (self.tasks or self.params or self.final_parallel_tasks):
-                raise MissingRequiredArgumentError(argument="'tasks' or 'params' or 'final_parallel_tasks'")
 
+        if not self.kind_dict and not self.yaml_file:
             self.res["spec"] = {}
-            if self.params:
-                self.res["spec"]["params"] = self.params
-            if self.tasks:
-                self.res["spec"]["tasks"] = self.tasks
-            if self.final_parallel_tasks:
-                self.res["spec"]["finally"] = self.final_parallel_tasks
+            _spec = self.res["spec"]
+
+            if self.description is not None:
+                _spec["description"] = self.description
+
+            if self.display_name is not None:
+                _spec["displayName"] = self.display_name
+
+            if self.finally_ is not None:
+                _spec["finally"] = self.finally_
+
+            if self.params is not None:
+                _spec["params"] = self.params
+
+            if self.results is not None:
+                _spec["results"] = self.results
+
+            if self.tasks is not None:
+                _spec["tasks"] = self.tasks
+
+            if self.workspaces is not None:
+                _spec["workspaces"] = self.workspaces
+
+    # End of generated code
