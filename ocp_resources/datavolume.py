3f59fa99ac3f033b11a73485be5cb9367819df80
Ahmad Hafe <ahafe@redhat.com>
2025-07-31 16:23:26 +0300

    increase time for pvund

--- ocp_resources/datavolume.py
+++ ocp_resources/datavolume.py
@@ -270,7 +270,8 @@ class DataVolume(NamespacedResource):
         self,
         timeout=TIMEOUT_10MINUTES,
         failure_timeout=TIMEOUT_2MINUTES,
-        dv_garbage_collection_enabled=False,
+        pvc_wait_for_bound_timeout=TIMEOUT_1MINUTE,
+        dv_garbage_collection_enabled=None,
         stop_status_func=None,
         *stop_status_func_args,
         **stop_status_func_kwargs,
@@ -281,7 +282,9 @@ class DataVolume(NamespacedResource):
         Args:
             timeout (int):  Time to wait for the DataVolume to succeed.
             failure_timeout (int): Time to wait for the DataVolume to have not Pending/None status
-            dv_garbage_collection_enabled (bool, default: False): if True, expect that DV will disappear after success
+            pvc_wait_for_bound_timeout (int): Time to wait for the PVC to reach 'Bound' status.
+            dv_garbage_collection_enabled (bool, default: None): DV garbage collection is deprecated and removed in
+            v4.19
             stop_status_func (function): function that is called inside the TimeoutSampler
                 if it returns True - stop the Sampler and raise TimeoutExpiredError
                 Example:
@@ -325,7 +328,7 @@ class DataVolume(NamespacedResource):
             raise
 
         # For CSI storage, PVC gets Bound after DV succeeded
-        return self.pvc.wait_for_status(status=PersistentVolumeClaim.Status.BOUND, timeout=TIMEOUT_1MINUTE)
+        return self.pvc.wait_for_status(status=PersistentVolumeClaim.Status.BOUND, timeout=pvc_wait_for_bound_timeout)
 
     def delete(self, wait=False, timeout=TIMEOUT_4MINUTES, body=None):
         """
