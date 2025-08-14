265c96ae165fa2e005bd89a1b7c9d0140ec117aa
Debarati Basu-Nag <dbasunag@redhat.com>
2025-08-14 11:09:28 +0200

    feat: Add ScaledObject resources (#2497)

--- class_generator/tests/manifests/Deployment/deployment.py
+++ class_generator/tests/manifests/Deployment/deployment.py
@@ -93,10 +93,9 @@ class Deployment(NamespacedResource):
               name value path port scheme enum: HTTP, HTTPS sleep seconds
               tcpSocket host port preStop exec command httpGet host httpHeaders
               name value path port scheme enum: HTTP, HTTPS sleep seconds
-              tcpSocket host port stopSignal enum: SIGABRT, SIGALRM, SIGBUS,
-              SIGCHLD, .... livenessProbe exec command failureThreshold grpc
-              port service httpGet host httpHeaders name value path port scheme
-              enum: HTTP, HTTPS initialDelaySeconds periodSeconds
+              tcpSocket host port livenessProbe exec command failureThreshold
+              grpc port service httpGet host httpHeaders name value path port
+              scheme enum: HTTP, HTTPS initialDelaySeconds periodSeconds
               successThreshold tcpSocket host port terminationGracePeriodSeconds
               timeoutSeconds name ports containerPort hostIP hostPort name
               protocol enum: SCTP, TCP, UDP readinessProbe exec command
@@ -132,10 +131,9 @@ class Deployment(NamespacedResource):
               name value path port scheme enum: HTTP, HTTPS sleep seconds
               tcpSocket host port preStop exec command httpGet host httpHeaders
               name value path port scheme enum: HTTP, HTTPS sleep seconds
-              tcpSocket host port stopSignal enum: SIGABRT, SIGALRM, SIGBUS,
-              SIGCHLD, .... livenessProbe exec command failureThreshold grpc
-              port service httpGet host httpHeaders name value path port scheme
-              enum: HTTP, HTTPS initialDelaySeconds periodSeconds
+              tcpSocket host port livenessProbe exec command failureThreshold
+              grpc port service httpGet host httpHeaders name value path port
+              scheme enum: HTTP, HTTPS initialDelaySeconds periodSeconds
               successThreshold tcpSocket host port terminationGracePeriodSeconds
               timeoutSeconds name ports containerPort hostIP hostPort name
               protocol enum: SCTP, TCP, UDP readinessProbe exec command
@@ -171,8 +169,7 @@ class Deployment(NamespacedResource):
               value path port scheme enum: HTTP, HTTPS sleep seconds tcpSocket
               host port preStop exec command httpGet host httpHeaders name value
               path port scheme enum: HTTP, HTTPS sleep seconds tcpSocket host
-              port stopSignal enum: SIGABRT, SIGALRM, SIGBUS, SIGCHLD, ....
-              livenessProbe exec command failureThreshold grpc port service
+              port livenessProbe exec command failureThreshold grpc port service
               httpGet host httpHeaders name value path port scheme enum: HTTP,
               HTTPS initialDelaySeconds periodSeconds successThreshold tcpSocket
               host port terminationGracePeriodSeconds timeoutSeconds name ports
@@ -202,14 +199,13 @@ class Deployment(NamespacedResource):
               subPathExpr workingDir nodeName nodeSelector os name overhead
               preemptionPolicy enum: Never, PreemptLowerPriority priority
               priorityClassName readinessGates conditionType resourceClaims name
-              resourceClaimName resourceClaimTemplateName resources claims name
-              request limits requests restartPolicy enum: Always, Never,
-              OnFailure runtimeClassName schedulerName schedulingGates name
-              securityContext appArmorProfile localhostProfile type enum:
-              Localhost, RuntimeDefault, Unconfined fsGroup fsGroupChangePolicy
-              enum: Always, OnRootMismatch runAsGroup runAsNonRoot runAsUser
-              seLinuxChangePolicy seLinuxOptions level role type user
-              seccompProfile localhostProfile type enum: Localhost,
+              resourceClaimName resourceClaimTemplateName restartPolicy enum:
+              Always, Never, OnFailure runtimeClassName schedulerName
+              schedulingGates name securityContext appArmorProfile
+              localhostProfile type enum: Localhost, RuntimeDefault, Unconfined
+              fsGroup fsGroupChangePolicy enum: Always, OnRootMismatch
+              runAsGroup runAsNonRoot runAsUser seLinuxOptions level role type
+              user seccompProfile localhostProfile type enum: Localhost,
               RuntimeDefault, Unconfined supplementalGroups
               supplementalGroupsPolicy enum: Merge, Strict sysctls name value
               windowsOptions gmsaCredentialSpec gmsaCredentialSpecName
---
+++ ocp_resources/scaled_object.py
@@ -0,0 +1,106 @@
+# Generated using https://github.com/RedHatQE/openshift-python-wrapper/blob/main/scripts/resource/README.md
+
+
+from typing import Any
+from ocp_resources.resource import NamespacedResource
+from ocp_resources.exceptions import MissingRequiredArgumentError
+
+
+class ScaledObject(NamespacedResource):
+    """
+    ScaledObject is a specification for a ScaledObject resource
+    """
+
+    api_group: str = NamespacedResource.ApiGroup.KEDA_SH
+
+    def __init__(
+        self,
+        advanced: dict[str, Any] | None = None,
+        cooldown_period: int | None = None,
+        fallback: dict[str, Any] | None = None,
+        idle_replica_count: int | None = None,
+        initial_cooldown_period: int | None = None,
+        max_replica_count: int | None = None,
+        min_replica_count: int | None = None,
+        polling_interval: int | None = None,
+        scale_target_ref: dict[str, Any] | None = None,
+        triggers: list[Any] | None = None,
+        **kwargs: Any,
+    ) -> None:
+        r"""
+        Args:
+            advanced (dict[str, Any]): AdvancedConfig specifies advance scaling options
+
+            cooldown_period (int): No field description from API
+
+            fallback (dict[str, Any]): Fallback is the spec for fallback options
+
+            idle_replica_count (int): No field description from API
+
+            initial_cooldown_period (int): No field description from API
+
+            max_replica_count (int): No field description from API
+
+            min_replica_count (int): No field description from API
+
+            polling_interval (int): No field description from API
+
+            scale_target_ref (dict[str, Any]): ScaleTarget holds the reference to the scale target Object
+
+            triggers (list[Any]): No field description from API
+
+        """
+        super().__init__(**kwargs)
+
+        self.advanced = advanced
+        self.cooldown_period = cooldown_period
+        self.fallback = fallback
+        self.idle_replica_count = idle_replica_count
+        self.initial_cooldown_period = initial_cooldown_period
+        self.max_replica_count = max_replica_count
+        self.min_replica_count = min_replica_count
+        self.polling_interval = polling_interval
+        self.scale_target_ref = scale_target_ref
+        self.triggers = triggers
+
+    def to_dict(self) -> None:
+        super().to_dict()
+
+        if not self.kind_dict and not self.yaml_file:
+            if self.scale_target_ref is None:
+                raise MissingRequiredArgumentError(argument="self.scale_target_ref")
+
+            if self.triggers is None:
+                raise MissingRequiredArgumentError(argument="self.triggers")
+
+            self.res["spec"] = {}
+            _spec = self.res["spec"]
+
+            _spec["scaleTargetRef"] = self.scale_target_ref
+            _spec["triggers"] = self.triggers
+
+            if self.advanced is not None:
+                _spec["advanced"] = self.advanced
+
+            if self.cooldown_period is not None:
+                _spec["cooldownPeriod"] = self.cooldown_period
+
+            if self.fallback is not None:
+                _spec["fallback"] = self.fallback
+
+            if self.idle_replica_count is not None:
+                _spec["idleReplicaCount"] = self.idle_replica_count
+
+            if self.initial_cooldown_period is not None:
+                _spec["initialCooldownPeriod"] = self.initial_cooldown_period
+
+            if self.max_replica_count is not None:
+                _spec["maxReplicaCount"] = self.max_replica_count
+
+            if self.min_replica_count is not None:
+                _spec["minReplicaCount"] = self.min_replica_count
+
+            if self.polling_interval is not None:
+                _spec["pollingInterval"] = self.polling_interval
+
+    # End of generated code
