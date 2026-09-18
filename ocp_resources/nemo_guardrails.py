c01b3505e036d266b2009ce7d84ee3215b88f1d8
Rob Geada <rob@geada.net>
2026-09-18 17:38:37 +0100

    feat(ocp_resources/nemo_guardrails): Update NeMo Guardrails to 3.6 CR spec
    Signed-off-by: Rob Geada <rob@geada.net>

--- ocp_resources/nemo_guardrails.py
+++ ocp_resources/nemo_guardrails.py
@@ -18,8 +18,10 @@ class NemoGuardrails(NamespacedResource):
         self,
         ca_bundle_config: dict[str, Any] | None = None,
         env: list[Any] | None = None,
+        expose_route: bool | None = None,
         nemo_configs: list[Any] | None = None,
         replicas: int | None = None,
+        template: dict[str, Any] | None = None,
         **kwargs: Any,
     ) -> None:
         r"""
@@ -29,19 +31,26 @@ class NemoGuardrails(NamespacedResource):
 
             env (list[Any]): Define Env information for the main container
 
+            expose_route (bool): ExposeRoute controls whether an external route to the NeMo Guardrails
+              server is created. Defaults to true.
+
             nemo_configs (list[Any]): NemoConfig should be the names of the configmaps containing NeMO
               server configuration files. All files in NemoConfigs will be
               mounted to /app/config/$Name
 
             replicas (int): Number of replicas for the NeMo Guardrails deployment
 
+            template (dict[str, Any]): Template describes the pod template used by the deployment
+
         """
         super().__init__(**kwargs)
 
         self.ca_bundle_config = ca_bundle_config
         self.env = env
+        self.expose_route = expose_route
         self.nemo_configs = nemo_configs
         self.replicas = replicas
+        self.template = template
 
     def to_dict(self) -> None:
 
@@ -51,10 +60,13 @@ class NemoGuardrails(NamespacedResource):
             if self.nemo_configs is None:
                 raise MissingRequiredArgumentError(argument="self.nemo_configs")
 
+
             self.res["spec"] = {}
             _spec = self.res["spec"]
 
+
             _spec["nemoConfigs"] = self.nemo_configs
+
 
             if self.ca_bundle_config is not None:
                 _spec["caBundleConfig"] = self.ca_bundle_config
@@ -62,7 +74,13 @@ class NemoGuardrails(NamespacedResource):
             if self.env is not None:
                 _spec["env"] = self.env
 
+            if self.expose_route is not None:
+                _spec["exposeRoute"] = self.expose_route
+
             if self.replicas is not None:
                 _spec["replicas"] = self.replicas
+
+            if self.template is not None:
+                _spec["template"] = self.template
 
     # End of generated code
